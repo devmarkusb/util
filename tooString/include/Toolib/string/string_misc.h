bb3817f576145250b5f33f363653b6ecf4a3c880
MarkusB <markusb81@web.de>
2016-12-17 14:05:40 +0100

    string: addded replaceAll functions

--- tooString/include/Toolib/string/string_misc.h
+++ tooString/include/Toolib/string/string_misc.h
@@ -1,4 +1,4 @@
-// Markus Borris, 2011
+// Markus Borris, 2011-16
 // This file is part of Toolib library.
 
 //!
@@ -10,9 +10,11 @@
 #ifndef STRING_MISC_H_INCL_ciubhwduibgwi37
 #define STRING_MISC_H_INCL_ciubhwduibgwi37
 
+#include "Toolib/assert.h"
 #include <algorithm>
 #include <ctype.h>
 #include <string>
+#include <vector>
 
 
 namespace too
@@ -30,7 +32,35 @@ inline std::string& makeLower(std::string& s)
     std::transform(s.begin(), s.end(), s.begin(), tolower);
     return s;
 }
+
+//! Expects \param fromSub non-empty.
+inline void replaceAll(std::string& s, const std::string& fromSub, const std::string& toSub)
+{
+    TOO_EXPECT(!fromSub.empty());
+    size_t start_pos{};
+    while ((start_pos = s.find(fromSub, start_pos)) != std::string::npos)
+    {
+        s.replace(start_pos, fromSub.length(), toSub);
+        start_pos += toSub.length();
+    }
 }
+
+//! Expects \param fromSub non-empty. Expects \params toSubs to be of at least the
+//! count of occurrences of \param fromSub within \param s.
+inline void replaceAll(std::string& s, const std::string& fromSub, const std::vector<std::string>& toSubs)
+{
+    TOO_EXPECT(!fromSub.empty());
+    size_t counter{};
+    size_t start_pos{};
+    while ((start_pos = s.find(fromSub, start_pos)) != std::string::npos)
+    {
+        TOO_ASSERT(toSubs.size() > counter);
+        s.replace(start_pos, fromSub.length(), toSubs[counter]);
+        start_pos += toSubs[counter].length();
+        ++counter;
+    }
+}
+} // str
 } // too
 
 #endif
