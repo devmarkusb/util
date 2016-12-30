1c29513cb6e78b0b226a9dba02477b891472a8ef
MarkusB <markusb81@web.de>
2016-12-30 16:30:18 +0100

    ignore_arg also for functions

--- tooBasicCodeSupport/include/Toolib/ignore_arg.h
+++ tooBasicCodeSupport/include/Toolib/ignore_arg.h
@@ -9,20 +9,27 @@
 #ifndef IGNORE_ARG_H_kasgfuznfgzwngfz27gwfnsdfsd
 #define IGNORE_ARG_H_kasgfuznfgzwngfz27gwfnsdfsd
 
+#include "Toolib/PPDEFS.h"
+
 
 namespace too
 {
-//! To 'suppress' compiler warning of an unused function parameter.
+//! To 'suppress' compiler warning of an unused function parameter. Beyond that, it is
+//! even usable for a function itself (e.g. gcc shows a warning for unused functions).
 /** Example:
     \code
     f(int i) { ignore_arg(i); }
     \endcode
-*/
+    Or:
+    \code
+    f() { ignore_arg(f); }
+    \endcode*/
 template <typename T>
 void ignore_arg(T&&)
 {
     // intentionally doing nothing; ideally the compiler optimizes away any call of this function
+    TOO_NOOP;
 }
-}
+} // too
 
 #endif
