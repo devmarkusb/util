24dcf43b03cb3546c1411f3871b8ddc79f842573
MarkusB <markusb81@web.de>
2017-01-22 02:11:30 +0100

    trace: added monstrous functionality, cf. code doc.

--- tooBasicCodeSupport/include/Toolib/trace.h
+++ tooBasicCodeSupport/include/Toolib/trace.h
@@ -11,11 +11,16 @@
 #define TRACE_H_INCL_dfsgjn854gcnz782x5g7813sdyfwh
 
 #include "Toolib/class/non_copyable.h"
+#include "Toolib/error.h"
 #include "Toolib/PPDEFS.h"
 #include "Toolib/std/std_extensions.h"
+#include <iomanip>
+#include <iostream>
 #include <memory>
 #include <sstream>
 #if TOO_OS_WINDOWS
+#define __STDC_WANT_LIB_EXT1__ 1
+#include <stdio.h>
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
@@ -24,42 +29,190 @@
 
 namespace too
 {
-namespace trace
+//! Purpose of too::trace is a quick&dirty substitute for logging. That is you can print to console window
+//! and/or IDE output window. For GUI applications you can even open an console window and redirect cout and cerr
+//! calls there. The initialization of the tracing is highly configurable, cf. too::trace::init.
+/** Usage:
+        #include "Toolib/trace.h" // for the file you want to trace something.
+
+        int main()
+        {
+            // first call init (and only once)
+            too::tracer::init(); // cf. function doc. for configuration possiblities
+            too::trace("ERROR") << "not so good, " << 42;
+            too::trace() << "not so good, " << 42; // does the same, since "ERROR" is also default
+            }
+    It is recommended to use level strings "ERROR", "WARN", "INFO", "TRACE" in that order of decreasing severity.
+    Note that you don't need to append '\n' to your trace line, it's done automatically.
+    Also note, that "ERROR" is default and can be left out.*/
+struct trace;
+
+namespace tracer
 {
-struct Enabled
-{
-};
-struct Disabled
-{
-};
-struct OutputToIDEWindow
-{
-};
-struct NoOutputToIDEWindow
-{
-};
 struct OutputToConsole
 {
-};
-struct NoOutputToConsole
-{
-};
-struct CheckConsoleOpen
-{
-};
-struct DontCheckConsoleOpen
-{
+    static bool trace(const std::ostringstream& ss)
+    {
+        std::cerr << ss.str();
+        return true;
+    }
 };
 
-struct Tracer
+struct NoOutputToConsole
 {
-    virtual ~Tracer() {}
+    static bool trace(const std::ostringstream&) { return false; }
+};
+
+struct OutputToIDEWindow
+{
+    template <class OutputToConsolePolicy>
+    static void trace(const std::ostringstream& ss)
+    {
+#if TOO_OS_WINDOWS
+#if TOO_COMP_MS_VISUAL_STUDIO_CPP
+        OutputDebugStringA(ss.str().c_str());
+#else
+        if (!OutputToConsolePolicy::trace(ss))
+            OutputDebugStringA(ss.str().c_str());
+#endif
+#else
+        throw too::not_implemented{"OutputToIDEWindow::trace"};
+#endif
+    }
+};
+
+struct NoOutputToIDEWindow
+{
+    template <class>
+    static void trace(const std::ostringstream&)
+    {
+        // Note: e.g. for mingw we can't prevent output from showing up in IDE window
+        TOO_NOOP;
+    }
+};
+
+struct DontAlsoBindStdout
+{
+    static void bind(bool& ret_bound) { ret_bound = false; }
+};
+
+struct AlsoBindStdout
+{
+    static void bind(bool& ret_bound)
+    {
+        ret_bound = false;
+        FILE* nsp{};
+        const auto open_failed = freopen_s(&nsp, "CONOUT$", "w", stdout);
+        if (!open_failed && nsp)
+            ret_bound = true;
+        else
+            TOO_ASSERT(false);
+    }
+};
+
+struct CheckConsoleOpen
+{
+    template <class AlsoBindStdoutToNewConsolePolicy = DontAlsoBindStdout>
+    static void openConsoleIfNecessary(bool& ret_stderrBound, bool& ret_stdoutBound)
+    {
+        ret_stderrBound = false;
+        ret_stdoutBound = false;
+
+        const auto alloc_ok = AllocConsole();
+        if (!alloc_ok)
+        {
+            TOO_ASSERT(false); // you don't need to try to open a new console, you already have one
+            return;
+        }
+
+        FILE* nsp{};
+        const auto open_failed = freopen_s(&nsp, "CONOUT$", "w", stderr);
+        if (!open_failed && nsp)
+            ret_stderrBound = true;
+        else
+            TOO_ASSERT(false);
+
+        AlsoBindStdoutToNewConsolePolicy::bind(ret_stdoutBound);
+    }
+};
+
+struct DontCheckConsoleOpen
+{
+    template <class>
+    static void openConsoleIfNecessary(bool&, bool&)
+    {
+        TOO_NOOP;
+    }
+};
+
+struct Enabled
+{
+    template <class OutputToIDEWindowPolicy, class OutputToConsolePolicy>
+    static void trace(const std::ostringstream& ss)
+    {
+        OutputToIDEWindowPolicy::trace<OutputToConsolePolicy>(ss);
+        OutputToConsolePolicy::trace(ss);
+    }
+};
+
+struct Disabled
+{
+    template <class, class>
+    static void trace(const std::ostringstream&)
+    {
+        TOO_NOOP;
+    }
+};
+
+//! This needs to be called before any call to too::trace and only once.
+//! That is yopu can fix your configuration only once and for all for the whole program.
+/** Configuration options in detail:
+        EnabledIfPolicy: switches tracing on/off entirely, e.g. useful to be bound to a
+                            macro define specifying the case of a release or deployment build
+        OutputToIDEWindowPolicy: choose whether you want to trace to your IDE output window;
+                            note that you can't switch off those traces in some environments if
+                            you switched on console traces at the same time
+                            (mingw+QtCreator is an example)
+        OutputToConsolePolicy: choose wheter to trace to a console window or not
+        CheckConsoleOpenPolicy: a check+open only makes sense for non-console GUI application;
+                            there you can open an additional console window to see traces if
+                            switched on
+        AlsoBindStdoutToNewConsolePolicy: only makes sense if you checked+opened a new console
+                            window by the previous policy; then youn have the option to not only
+                            bind stderr (cerr) - which is done by default and used for traces
+                            to the console - but also stdout (cout) to the console window output.*/
+template <class EnabledIfPolicy = Enabled, class OutputToIDEWindowPolicy = OutputToIDEWindow,
+    class OutputToConsolePolicy = NoOutputToConsole, class CheckConsoleOpenPolicy = DontCheckConsoleOpen,
+    class AlsoBindStdoutToNewConsolePolicy = DontAlsoBindStdout>
+// EnabledIfPolicy expected to be Enabled or Disabled
+// OutputToIDEWindowPolicy expected to be OutputToIDEWindow or NoOutputToIDEWindow
+// OutputToConsolePolicy expected to be OutputToConsole or NoOutputToConsole
+// CheckConsoleOpenPolicy expected to be CheckConsoleOpen or DontCheckConsoleOpen
+// AlsoBindStdoutToNewConsolePolicy expected to be AlsoBindStdout or DontAlsoBindStdout
+inline void init()
+{
+    if (detail_impl::StreamTracerWrapperSingleton::getInstance().tracer())
+        throw std::runtime_error{"trace init called more often than once"};
+
+    bool ret_stderrBound{};
+    bool ret_stdoutBound{};
+    CheckConsoleOpenPolicy::openConsoleIfNecessary<AlsoBindStdoutToNewConsolePolicy>(ret_stderrBound, ret_stdoutBound);
+
+    detail_impl::StreamTracerWrapperSingleton::getInstance().tracer() = too::make_unique<
+        detail_impl::StreamTracer_impl<EnabledIfPolicy, OutputToIDEWindowPolicy, OutputToConsolePolicy>>(
+        ret_stderrBound, ret_stdoutBound);
+};
+
+namespace detail_impl
+{
+struct StreamTracer
+{
+    virtual ~StreamTracer() {}
     virtual void trace(const std::ostringstream&) const = 0;
 };
 
-
 template <class T>
-inline Tracer& operator<<(Tracer& t, const T& x)
+inline StreamTracer& operator<<(StreamTracer& t, const T& x)
 {
     std::ostringstream ss;
     ss << x;
@@ -67,12 +220,12 @@ inline Tracer& operator<<(Tracer& t, const T& x)
     return t;
 }
 
-struct End
+struct StreamTraceEndOfLine
 {
 };
 
 template <>
-inline Tracer& operator<<(Tracer& t, const End&)
+inline StreamTracer& operator<<(StreamTracer& t, const StreamTraceEndOfLine&)
 {
     std::ostringstream ss;
     ss << '\n';
@@ -81,69 +234,76 @@ inline Tracer& operator<<(Tracer& t, const End&)
 }
 
 template <class EnabledIfPolicy, class OutputToIDEWindowPolicy = OutputToIDEWindow,
-    class OutputToConsolePolicy = NoOutputToConsole, class CheckConsoleOpenPolicy = DontCheckConsoleOpen>
-struct Tracer_impl : public Tracer
+    class OutputToConsolePolicy = NoOutputToConsole>
+struct StreamTracer_impl : public StreamTracer
 {
-    virtual void trace(const std::ostringstream& ss) const override { std::cerr << ss.str(); }
+    StreamTracer_impl(bool close_stderr, bool close_stdout) : close_stderr{close_stderr}, close_stdout{close_stdout} {}
+
+    ~StreamTracer_impl()
+    {
+        if (close_stderr)
+            fclose(stderr);
+        if (close_stdout)
+            fclose(stdout);
+    }
+
+    virtual void trace(const std::ostringstream& ss) const override
+    {
+        EnabledIfPolicy::trace<OutputToIDEWindowPolicy, OutputToConsolePolicy>(ss);
+    }
 
 private:
-    std::ostringstream m_buffer;
+    bool close_stderr{};
+    bool close_stdout{};
 };
 
-
-template <class EnabledIfPolicy, class OutputToIDEWindowPolicy = OutputToIDEWindow,
-    class OutputToConsolePolicy = NoOutputToConsole, class CheckConsoleOpenPolicy = DontCheckConsoleOpen>
-inline void init()
+struct StreamTracerWrapperSingleton
 {
-    if (TracerWrapperSingleton::getInstance().tracer())
-        throw std::runtime_error{"trace init called more often than once"};
-    TracerWrapperSingleton::getInstance().tracer() = too::make_unique<
-        Tracer_impl<EnabledIfPolicy, OutputToIDEWindowPolicy, OutputToConsolePolicy, CheckConsoleOpenPolicy>>();
-};
-
-struct TracerWrapperSingleton
-{
-    static TracerWrapperSingleton& getInstance()
+    static StreamTracerWrapperSingleton& getInstance()
     {
-        static TracerWrapperSingleton inst;
+        static StreamTracerWrapperSingleton inst;
         return inst;
     }
 
-    std::unique_ptr<Tracer>& tracer() { return t; }
+    std::unique_ptr<StreamTracer>& tracer() { return t; }
 
 private:
-    std::unique_ptr<Tracer> t;
+    std::unique_ptr<StreamTracer> t;
 
-    TracerWrapperSingleton() = default;
+    StreamTracerWrapperSingleton() = default;
 };
 
-inline Tracer& stream()
+inline StreamTracer& stream()
 {
-    // if you crash here, you probably forgot to call too::trace::init before your first trace
-    return *TracerWrapperSingleton::getInstance().tracer();
+    // if you crash here, you probably forgot to call too::tracer::init before your first trace
+    return *StreamTracerWrapperSingleton::getInstance().tracer();
 }
+} // detail_impl
+} // tracer
 
-struct AutoEnd : private too::non_copyable
+struct trace : private too::non_copyable
 {
+    explicit trace(const std::string& level = "ERROR") : stream_{too::tracer::detail_impl::stream()}
+    {
+        std::ostringstream tmp;
+        tmp << std::left << std::setw(6) << level;
+        this->stream_ << tmp.str();
+    }
+
+    ~trace() { this->stream_ << tracer::detail_impl::StreamTraceEndOfLine{}; }
+
+    tracer::detail_impl::StreamTracer& stream() const { return this->stream_; }
+
 private:
-    mutable Tracer& m_obj;
-
-public:
-    explicit AutoEnd(Tracer& obj) : m_obj(obj) {}
-    ~AutoEnd() { m_obj << End{}; }
-
-    Tracer& obj() const { return m_obj; }
+    mutable tracer::detail_impl::StreamTracer& stream_;
 };
 
 template <typename T>
-const AutoEnd& operator<<(const AutoEnd& ae, T&& arg)
+const trace& operator<<(const trace& t, T&& arg)
 {
-    ae.obj() << std::forward<T>(arg);
-    return ae;
+    t.stream() << std::forward<T>(arg);
+    return t;
 }
-
-#define TOO_TRACE /*(level)*/ too::trace::AutoEnd(too::trace::stream(/*TOO_STRINGIFY(level)*/))
-} // trace
 
 
 namespace deprecated
