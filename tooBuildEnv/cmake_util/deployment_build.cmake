d8c68308ecb4d98145edbda6b4decf12cf8af026
MarkusB <markusb81@web.de>
2017-03-31 20:48:26 +0200

    added deploy target cmake choice

--- tooBuildEnv/cmake_util/deployment_build.cmake
+++ tooBuildEnv/cmake_util/deployment_build.cmake
@@ -1,1 +1,8 @@
 option(DEPLOYMENT_BUILD "do a final release build for deployment" $ENV{DeployArtifacts})
+
+set(DEPLOY_TARGET "" CACHE STRING "Special deployment choice (if OS is not sufficient) out of: winstore, ... or download (default, also if left empty)")
+if (DEPLOY_TARGET STREQUAL "winstore")
+    add_definitions(-DTOO_DEPLOY_TO_WINSTORE)
+#elseif (DEPLOY_TARGET STREQUAL "steam")
+#    add_definitions(-DTOO_DEPLOY_TO_STEAM)
+endif ()
