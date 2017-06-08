bb6521036d1b92b710887fa3c23d835639a1dcc6
MarkusB <markusb81@web.de>
2017-06-08 21:28:11 +0200

    cmake_util: more convenience for quick UWP builds: assets contain ready to use ico's as well now

--- tooBuildEnv/cmake_util/uwp_tools.cmake
+++ tooBuildEnv/cmake_util/uwp_tools.cmake
@@ -8,6 +8,7 @@ endif ()
 
 # Usage (e.g.):
 #   obtain_UWP_resources("348B82EC-3E83-499C-91B3-A2DAB20E041A" "${CMAKE_SOURCE_DIR}/installer/_content_for_bindir" "#336699" TargetAppName_UWP_ASSETS)
+#   For quick tests you can also use ${TOO_CMAKE_INC_UWP_TOOLS_DEF_ICO} for the _ico parent path.
 function(obtain_UWP_resources PACKAGE_GUID FULLPATH_TO_ico_PARENTDIR BackgroundColor retRESOURCES)
     if ("${DEPLOY_TARGET}" STREQUAL "uwp")
         set(APP_MANIFEST_NAME package.appxmanifest)
