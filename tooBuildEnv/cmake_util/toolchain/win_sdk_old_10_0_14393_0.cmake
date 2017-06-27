454942898691cf2b601fd3303ac72f0fd720781a
MarkusB <markusb81@web.de>
2017-06-28 00:10:24 +0200

    added toolchain file for building with older Windows 10 SDK on newer host environment (CMake otherwise assumes newest possible SDK and e.g. msvc2015 doesn't support those starting with versions > 14339)

---
+++ tooBuildEnv/cmake_util/toolchain/win_sdk_old_10_0_14393_0.cmake
@@ -0,0 +1,5 @@
+# Needed e.g. if you want to build with msvc2015 (supports no newer SDK than this) on a host that has
+# a newer SDK installed and also runs at the according Windows version. Then CMake would choose the most
+# modern possible SDK.
+
+set(CMAKE_SYSTEM_VERSION 10.0.14393.0)
