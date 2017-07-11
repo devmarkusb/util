9a81d3e2bc90dbfdc25086d7ffd2a0c9465365dc
MarkusB <markusb81@web.de>
2017-07-11 17:26:18 +0200

    uwp build fix

---
+++ tooBuildEnv/cmake_util/toolchain/detail/winrt_uwp_core.cmake
@@ -0,0 +1,6 @@
+# Provides mandatory configuration foundations you need to set up UWP projects properly (at least working for MSVC generator).
+
+set(CMAKE_SYSTEM_NAME WindowsStore)
+
+set(DEPLOY_TARGET "uwp" CACHE STRING "Don't change! UWP is already strictly chosen due to toolchain file.")
+set(BIN_PACKAGE_SUBDIR "AppX" CACHE STRING "Folder below the binary output directory that contains the actual deployment package. (Needs to be AppX - not yet clear how to customize for real.)")
--- tooBuildEnv/cmake_util/toolchain/winrt_uwp.cmake
+++ tooBuildEnv/cmake_util/toolchain/winrt_uwp.cmake
@@ -1,7 +1,4 @@
 # Provides mandatory configuration foundations you need to set up UWP projects properly (at least working for MSVC generator).
 
-set(CMAKE_SYSTEM_NAME WindowsStore)
 set(CMAKE_SYSTEM_VERSION 10.0)
-
-set(DEPLOY_TARGET "uwp" CACHE STRING "Don't change! UWP is already strictly chosen due to toolchain file.")
-set(BIN_PACKAGE_SUBDIR "AppX" CACHE STRING "Folder below the binary output directory that contains the actual deployment package. (Needs to be AppX - not yet clear how to customize for real.)")
+include(${CMAKE_CURRENT_LIST_DIR}/detail/winrt_uwp_core.cmake)
