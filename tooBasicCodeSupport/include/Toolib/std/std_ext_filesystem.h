cc3a5d1f0e98d3002089604eec52ad500edba6bb
MarkusB <markusb81@web.de>
2017-07-27 23:34:29 +0200

    added std extension for filesystem (can use the experimental/tr2 version if available or boost)

---
+++ tooBasicCodeSupport/include/Toolib/std/std_ext_filesystem.h
@@ -0,0 +1,31 @@
+// Markus Borris, 2017
+// This file is part of Toolib library.
+
+//!
+/** Cf. std_extensions.h
+    */
+//! \file
+
+#ifndef STD_EXT_FILESYSTEM_H_wueihfiwufy3u48732t
+#define STD_EXT_FILESYSTEM_H_wueihfiwufy3u48732t
+
+#include "Toolib/PPDEFS.h"
+#if TOO_OS_WINDOWS_UWP_APP && TOO_COMP_MS_VISUAL_STUDIO_CPP && TOO_COMP_MS_VS_VER == 1910
+#include <experimental/filesystem>
+namespace too
+{
+namespace std_fs = std::tr2::sys;
+}
+#elif !TOO_IS_BOOST_DISABLED
+#include "Toolib/PPDefs/GCC/SUPPRESS_WARNING_deprecated-declarations_BEGIN"
+#include "Toolib/PPDefs/GCC/SUPPRESS_WARNING_unused-variable_BEGIN"
+#include <boost/filesystem/operations.hpp>
+#include "Toolib/PPDefs/GCC/SUPPRESS_WARNING_END"
+#include <boost/filesystem/path.hpp>
+namespace too
+{
+namespace std_fs = boost::filesystem;
+}
+#endif
+
+#endif
