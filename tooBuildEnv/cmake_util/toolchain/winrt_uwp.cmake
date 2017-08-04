43c1c9ef73ad82f05684507fdd6a675e82573cfe
MarkusB <markusb81@web.de>
2017-08-04 14:43:19 +0200

    fix: couldn't find the compiler anymore (seems one needs to be more precise with the windows 10.x version number - but we prefer not to build on the newest version anyway)

--- tooBuildEnv/cmake_util/toolchain/winrt_uwp.cmake
+++ tooBuildEnv/cmake_util/toolchain/winrt_uwp.cmake
@@ -1,4 +1,4 @@
 # Provides mandatory configuration foundations you need to set up UWP projects properly (at least working for MSVC generator).
 
-set(CMAKE_SYSTEM_VERSION 10.0)
+set(CMAKE_SYSTEM_VERSION 10.0.14393.0)
 include(${CMAKE_CURRENT_LIST_DIR}/detail/winrt_uwp_core.cmake)
