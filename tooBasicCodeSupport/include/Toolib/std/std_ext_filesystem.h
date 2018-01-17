19093527779a54e825957368ef28e264fd65eaec
MarkusB <markusb81@web.de>
2018-01-17 21:48:46 +0100

    adaption to state of filesys from msvc version to msvc version

--- tooBasicCodeSupport/include/Toolib/std/std_ext_filesystem.h
+++ tooBasicCodeSupport/include/Toolib/std/std_ext_filesystem.h
@@ -10,11 +10,14 @@
 #define STD_EXT_FILESYSTEM_H_wueihfiwufy3u48732t
 
 #include "Toolib/PPDEFS.h"
-#if TOO_OS_WINDOWS_UWP_APP && TOO_COMP_MS_VISUAL_STUDIO_CPP && TOO_COMP_MS_VS_VER == 1910
+#if TOO_OS_WINDOWS_UWP_APP && TOO_COMP_MS_VISUAL_STUDIO_CPP
+#if TOO_COMP_MS_VS_VER != 1912
+#error "this might need an adpation"
+#endif
 #include <experimental/filesystem>
 namespace too
 {
-namespace std_fs = std::tr2::sys;
+namespace std_fs = std::experimental::filesystem::v1;
 }
 #elif !TOO_IS_BOOST_DISABLED
 #include "Toolib/PPDefs/GCC/SUPPRESS_WARNING_deprecated-declarations_BEGIN"
