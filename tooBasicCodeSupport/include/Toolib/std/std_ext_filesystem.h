26f6e2304dd109cd951ad368265a2281f9c17a85
Markus <markusb81@web.de>
2018-06-24 21:28:46 +0200

    dropped support of filesystem lib workarounds for older compilers

--- tooBasicCodeSupport/include/Toolib/std/std_ext_filesystem.h
+++ tooBasicCodeSupport/include/Toolib/std/std_ext_filesystem.h
@@ -10,25 +10,11 @@
 #define STD_EXT_FILESYSTEM_H_wueihfiwufy3u48732t
 
 #include "Toolib/PPDEFS.h"
-#if TOO_OS_WINDOWS_UWP_APP && TOO_COMP_MS_VISUAL_STUDIO_CPP
-#if TOO_COMP_MS_VS_VER != 1912
-#error "this might need an adpation"
-#endif
-#include <experimental/filesystem>
+
+#include <filesystem>
 namespace too
 {
-namespace std_fs = std::experimental::filesystem::v1;
+namespace std_fs = std::filesystem;
 }
-#elif !TOO_IS_BOOST_DISABLED
-#include "Toolib/PPDefs/GCC/SUPPRESS_WARNING_deprecated-declarations_BEGIN"
-#include "Toolib/PPDefs/GCC/SUPPRESS_WARNING_unused-variable_BEGIN"
-#include <boost/filesystem/operations.hpp>
-#include "Toolib/PPDefs/GCC/SUPPRESS_WARNING_END"
-#include <boost/filesystem/path.hpp>
-namespace too
-{
-namespace std_fs = boost::filesystem;
-}
-#endif
 
 #endif
