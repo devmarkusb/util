f977f5721fcb098d628d43bdfc70308008b871ea
MarkusB <markusb81@web.de>
2018-08-12 14:14:04 +0200

    added own filesystem implementation (+ tests, conforming to standard) fragments (those that are currently needed) in case we don't have access to that feature yet (e.g. because of Android NDK version)

--- basiccodesupport/include/toolib/std/std_ext_filesystem.h
+++ basiccodesupport/include/toolib/std/std_ext_filesystem.h
@@ -1,4 +1,4 @@
-// Markus Borris, 2017
+// Markus Borris, 2017-18
 // This file is part of tfl library.
 
 //!
@@ -10,7 +10,10 @@
 #define STD_EXT_FILESYSTEM_H_wueihfiwufy3u48732t
 
 #include "toolib/PPDEFS.h"
+#include <fstream>
 
+#if __has_include(<filesystem>)
+// Note, no fallback to the experimental versions anymore.
 #include <filesystem>
 namespace too
 {
@@ -19,6 +22,468 @@ namespace std_fs = std::experimental::filesystem;
 #else
 namespace std_fs = std::filesystem;
 #endif
+} // too
+
+#else
+
+//####################################################################################################################
+// A really quick&dirty implementation for the worst case not having the filesystem lib.
+
+#if TOO_OS_UNIX
+#include <dirent.h>
+#include <linux/limits.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#endif
+#include "toolib/assert.h"
+#include "toolib/ignore_arg.h"
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <sstream>
+#include <string>
+#include <system_error>
+
+namespace too
+{
+namespace std_fs
+{
+/** Current deviations from standard:
+    - The class can only handle paths with single distinct separators ("/", no repeating "//" ones).
+    Notes:
+    - The empty path is equivalent to a single dot '.', which represents the current dir, a relative path.
+    - For a file starting with  dot the starting dot belongs to the filename.*/
+struct path
+{
+    static const auto preferred_separator = '/';
+
+    //! Constructs an invalid path.
+    path() noexcept = default;
+    //! Expects \param s to be empty or a syntactically valid path with OS conforming or portable separators. Implicit!
+    path(std::string s) : s_{std::move(s)} {}
+    path(const char* s) : s_{s} {}
+
+    path& make_preferred() { return *this; }
+    const char* c_str() const noexcept { return s_.c_str(); }
+    std::string string() const
+    {
+        return s_;
+    }
+
+    std::string generic_string() const
+    {
+        return string();
+    }
+
+    bool empty() const noexcept
+    {
+        return s_.empty();
+    }
+
+    path extension() const
+    {
+        const size_t start = s_.rfind('.');
+        if (start == 0 || start == std::string::npos || s_[start - 1] == '/')
+            return {};
+        return path{s_.substr(start, s_.size() - start)};
+    }
+
+    path stem() const
+    {
+        const size_t end = s_.rfind('.');
+        size_t start = s_.rfind('/');
+
+        if (start == std::string::npos)
+            start = 0;
+        else
+            ++start;
+
+        const size_t maxLength = s_.size() - start;
+        size_t length = end == std::string::npos ? (s_.size() - start) : (end - start);
+        if (!length)
+            length = maxLength;
+
+        return path{s_.substr(start, length)};
+    }
+
+    path filename() const
+    {
+        size_t start = s_.rfind('/');
+
+        if (start == std::string::npos)
+            start = 0;
+        else
+            ++start;
+
+        return path{s_.substr(start, s_.size())};
+    }
+
+    path parent_path() const
+    {
+        size_t length = s_.rfind('/');
+
+        if (length == std::string::npos)
+            return {};
+        else if (length == 0)
+            ++length;
+
+        return path{s_.substr(0, length)};
+    }
+
+    path& remove_trailing_separator() noexcept
+    {
+        if (!s_.empty() && s_.back() == '/')
+            s_.pop_back();
+        return *this;
+    }
+
+    //! \Param replacement replaces the current extension *including* the dot.
+    path& replace_extension(const path& replacement = {})
+    {
+        const size_t start = s_.rfind('.');
+        if (start == 0 || start == std::string::npos || s_[start - 1] == '/')
+            s_ += replacement.string();
+        else
+            s_.replace(std::begin(s_) + start, std::end(s_), replacement.string());
+
+        return *this;
+    }
+
+    //! 'concat'
+    path& operator+=(const path& other)
+    {
+        s_ += other.s_;
+        return *this;
+    }
+
+    path& operator+=(char c)
+    {
+        s_ += c;
+        return *this;
+    }
+
+    //! 'append'
+    path& operator/=(const path& other)
+    {
+        if (!empty() && s_.back() != '/')
+            s_ += '/';
+        s_ += other.string();
+        return *this;
+    }
+
+    void clear() noexcept
+    {
+        s_.clear();
+    }
+
+private:
+    std::string s_;
+
+    path normalize() const
+    {
+        path ret{*this};
+        ret.normalize();
+        return ret;
+    }
+
+    void normalize()
+    {
+        if (s_ == ".")
+            clear();
+    }
+
+    friend inline bool operator==(const path& lhs, const path& rhs);
+};
+
+inline bool operator==(const path& lhs, const path& rhs)
+{
+    return lhs.string() == rhs.string();
 }
+
+inline bool operator!=(const path& lhs, const path& rhs)
+{
+    return !(lhs == rhs);
+}
+
+inline path operator+(const path& lhs, const path& rhs)
+{
+    path ret{lhs};
+    ret += rhs;
+    return ret;
+}
+
+inline path operator/(const path& lhs, const path& rhs)
+{
+    path ret{lhs};
+    ret /= rhs;
+    return ret;
+}
+
+std::ostream& operator<<(std::ostream& os, const path& p)
+{
+    os << p.string();
+    return os;
+}
+
+struct directory_entry
+{
+    explicit directory_entry(path p) : p_{std::move(p)} {}
+
+    void assign(const path& p) { p_ = p; }
+    //! Implicit conversion.
+    operator const path& () const noexcept { return p_; }
+
+private:
+    std_fs::path p_;
+
+    friend inline bool operator==(const directory_entry& lhs, const directory_entry& rhs);
+};
+
+inline bool operator==(const directory_entry& lhs, const directory_entry& rhs)
+{
+    return lhs.p_ == rhs.p_;
+}
+
+inline bool operator!=(const directory_entry& lhs, const directory_entry& rhs)
+{
+    return !(lhs == rhs);
+}
+
+#if TOO_OS_LINUX
+struct directory_iterator : public std::iterator<std::input_iterator_tag, directory_entry>
+{
+    //! Constructs an iterator pointing behind the last element.
+    directory_iterator() = default;
+    explicit directory_iterator(path p) : p_{std::move(p)}
+    {
+        init_dir();
+        next_entry();
+    }
+
+    ~directory_iterator() = default;
+
+    const directory_entry& operator*() const
+    {
+        return entry_;
+    }
+
+    const directory_entry* operator->() const
+    {
+        return &entry_;
+    }
+
+    directory_iterator& operator++()
+    {
+        next_entry();
+
+        return *this;
+    }
+
+private:
+    struct DIR_scoped
+    {
+        explicit DIR_scoped(path p) : p_{std::move(p)}
+        {
+            d_ = opendir(p_.c_str());
+            if (!d_)
+            {
+                std::stringstream ss;
+                ss << "could not open dir, error code: " << errno << ", dir: " << p_.string();
+                throw std::runtime_error{ss.str()};
+            }
+            TOO_ENSURE(d_);
+        }
+
+        ~DIR_scoped()
+        {
+            if (d_ && closedir(d_))
+            {
+                // Ignoring the error in release. 1) Don't want to make the destructor noexcept(false) as the class
+                // is designed to be used with the std heavily (which is designed for non-throwing destructors, meaning
+                // terminating programs, which is way too much here). 2) A lib shouldn't write to cerr.
+#ifndef NDEBUG
+                std::stringstream ss;
+                ss << "could not close dir, error code: " << errno << ", dir: " << p_.string();
+                std::string errmsg{ss.str()};
+                too::ignore_arg(errmsg);
+#endif
+                TOO_ASSERT(false);
+            }
+        }
+
+        DIR* get() const noexcept
+        {
+            return d_;
+        }
+
+        void clear() noexcept
+        {
+            p_.clear();
+            d_ = {};
+        };
+
+    private:
+        path p_;
+        DIR* d_{};
+    };
+
+    mutable path p_;
+    mutable std::shared_ptr<DIR_scoped> d_;
+    mutable struct dirent* entry_impl_{};
+    mutable directory_entry entry_{{}};
+
+    void init_dir() const
+    {
+        TOO_EXPECT(!d_);
+        d_ = std::make_shared<DIR_scoped>(p_);
+        TOO_ENSURE(d_);
+    }
+
+    void next_entry() const
+    {
+        const auto errno_prev = errno;
+        do
+        {
+            entry_impl_ = readdir(d_->get());
+        }
+        while (entry_impl_ && (
+                (entry_impl_->d_name[0] == '.' && entry_impl_->d_name[1] == 0) ||
+                (entry_impl_->d_name[0] == '.' && entry_impl_->d_name[1] == '.' && entry_impl_->d_name[2] == 0)));
+
+        if (entry_impl_)
+            entry_.assign(p_ / path{entry_impl_->d_name});
+        else
+        {
+            if (errno != errno_prev)
+            {
+                std::stringstream ss;
+                ss << "could not read dir, error code: " << errno << ", parent dir: " << p_.string();
+                throw std::runtime_error{ss.str()};
+            }
+            p_.clear();
+            d_->clear();
+            entry_impl_ = {};
+            entry_.assign({});
+        }
+    }
+
+    friend bool operator==(const directory_iterator& lhs, const directory_iterator& rhs);
+};
+
+inline bool operator==(const directory_iterator& lhs, const directory_iterator& rhs)
+{
+    return lhs.entry_ == rhs.entry_;
+}
+
+inline bool operator!=(const directory_iterator& lhs, const directory_iterator& rhs)
+{
+    return !(lhs == rhs);
+}
+
+#else
+#error "not implemented"
+#endif
+
+inline bool exists(const path& p)
+{
+#if TOO_OS_LINUX
+    struct stat dummy{};
+    return (stat(p.c_str(), &dummy) == 0);
+#else
+#error "not implemented"
+#endif
+}
+
+//! \Returns false if already existing or error occurred.
+inline bool create_directories(const path& p)
+{
+#if TOO_OS_LINUX
+    auto dir{p.c_str()};
+    char tmp[256]{};
+    size_t len{};
+
+    snprintf(tmp, sizeof(tmp), "%s", dir);
+    len = strlen(tmp);
+    if (tmp[len - 1] == '/')
+        tmp[len - 1] = 0;
+    for (char* p_ = tmp + 1; *p_; p_++)
+    {
+        if (*p_ == '/')
+        {
+            *p_ = 0;
+            mkdir(tmp, S_IRWXU);
+            *p_ = '/';
+        }
+    }
+    return mkdir(tmp, S_IRWXU) == 0;
+#else
+#error "not implemented"
+#endif
+}
+
+inline bool is_regular_file(const path& p)
+{
+#if TOO_OS_LINUX
+    struct stat path_stat{};
+    stat(p.c_str(), &path_stat);
+    return S_ISREG(path_stat.st_mode);
+#else
+#error "not implemented"
+#endif
+}
+
+inline bool remove(const path& p)
+{
+#if TOO_OS_LINUX
+    return ::remove(p.c_str()) == 0;
+#else
+#error "not implemented"
+#endif
+}
+
+inline path current_path(std::error_code& ec)
+{
+#if TOO_OS_LINUX
+    char cwd[PATH_MAX]{};
+    if (getcwd(cwd, sizeof(cwd)))
+    {
+        return path{std::string{cwd}};
+    }
+    else
+    {
+        ec.assign(errno, std::system_category());
+        return {};
+    }
+#else
+#error "not implemented"
+#endif
+}
+
+inline void current_path(const path& p, std::error_code& ec) noexcept
+{
+#if TOO_OS_LINUX
+    if (chdir(p.c_str()))
+        ec.assign(errno, std::system_category());
+#else
+#error "not implemented"
+#endif
+}
+} // std_fs
+} // too
+
+#endif
+
+namespace too
+{
+namespace file
+{
+inline void touch(const too::std_fs::path& p)
+{
+    std::ofstream f{p.string()};
+}
+} // file
+} // too
 
 #endif
--- basiccodesupport/include/toolib/std/std_ext_filesystem.test.cpp
+++ basiccodesupport/include/toolib/std/std_ext_filesystem.test.cpp
@@ -1,2 +1,363 @@
-#define TOO_IS_BOOST_DISABLED   1
+/** Includes some physical tests on the filesystem. The playground are subdirs under root's tmp dir.
+    File/dir removals/overwrites are fully avoided! */
 #include "std_ext_filesystem.h"
+#include "toolib/finally.h"
+#include <algorithm>
+#include <chrono>
+#include <string>
+#include <thread>
+#include "gtest/gtest.h"
+
+namespace fs = too::std_fs;
+
+namespace
+{
+namespace
+{
+const std::string physical_test_dir_s{"/tmp/tfl/test"};
+} // namespace
+const fs::path physical_test_dir{physical_test_dir_s};
+
+//! \Returns finally reversed digits of increasing numbers. Source is a steady clock of the system in milliseconds.
+/** There is also a helping sleep for 1ms included. Features preferred changing of leading digits.
+    Note, this implies a decrease every tenth step and possible leading zeros.*/
+std::string getSteadyUniqueNr()
+{
+    std::string ret = std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::steady_clock::now().time_since_epoch()).count());
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    std::reverse(std::begin(ret), std::end(ret));
+    return ret;
+}
+} // namespace
+
+TEST(pathTest, constr_assign)
+{
+    fs::path p;
+    p = "foo";
+    EXPECT_EQ(p, fs::path{"foo"});
+    p = std::string{"bar"};
+    EXPECT_EQ(p, fs::path{"bar"});
+}
+
+TEST(pathTest, empty)
+{
+    const fs::path empty;
+    EXPECT_TRUE(empty.empty());
+    const fs::path dot{"."};
+    EXPECT_TRUE(!dot.empty());
+}
+
+TEST(pathTest, extension)
+{
+    const fs::path empty;
+    EXPECT_EQ(empty.extension(), empty);
+
+    const fs::path usual{"/home/x/test.txt"};
+    EXPECT_EQ(usual.extension(), fs::path{".txt"});
+
+    const fs::path empty_ext{"/home/x/test"};
+    EXPECT_EQ(empty_ext.extension(), empty);
+
+    const fs::path dot{"."};
+    EXPECT_EQ(dot.extension(), empty);
+
+    const fs::path empty_ext_dot{"/home/x/test."};
+    EXPECT_EQ(empty_ext_dot.extension(), dot);
+
+    const fs::path ext_only{"/tmp/.sh"};
+    EXPECT_EQ(ext_only.extension(), empty);
+
+    const fs::path usual_justfile{"file.extension"};
+    EXPECT_EQ(usual_justfile.extension(), fs::path{".extension"});
+}
+
+TEST(pathTest, stem)
+{
+    const fs::path empty;
+    EXPECT_EQ(empty.stem(), empty);
+
+    const fs::path usual{"/home/x/test.txt"};
+    EXPECT_EQ(usual.stem(), fs::path{"test"});
+
+    const fs::path empty_ext{"/home/x/test"};
+    EXPECT_EQ(empty_ext.stem(), fs::path{"test"});
+
+    const fs::path dot{"."};
+    EXPECT_EQ(dot.stem(), fs::path{"."});
+
+    const fs::path empty_ext_dot{"/home/x/test."};
+    EXPECT_EQ(empty_ext_dot.stem(), fs::path{"test"});
+
+    const fs::path ext_only{"/tmp/.sh"};
+    EXPECT_EQ(ext_only.stem(), fs::path{".sh"});
+
+    const fs::path usual_justfile{"file.extension"};
+    EXPECT_EQ(usual_justfile.stem(), fs::path{"file"});
+}
+
+TEST(pathTest, stem_ext_example)
+{
+    fs::path p{"foo.bar.baz.tar"};
+    EXPECT_EQ(p.extension(), fs::path{".tar"});
+    p = p.stem();
+    EXPECT_EQ(p.extension(), fs::path{".baz"});
+    p = p.stem();
+    EXPECT_EQ(p.extension(), fs::path{".bar"});
+    p = p.stem();
+    EXPECT_TRUE(p.extension().empty());
+}
+
+TEST(pathTest, filename)
+{
+    const fs::path empty;
+    EXPECT_EQ(empty.filename(), empty);
+
+    const fs::path usual{"/home/x/test.txt"};
+    EXPECT_EQ(usual.filename(), fs::path{"test.txt"});
+
+    const fs::path empty_ext{"/home/x/test"};
+    EXPECT_EQ(empty_ext.filename(), fs::path{"test"});
+
+    const fs::path dot{"."};
+    EXPECT_EQ(dot.filename(), fs::path{"."});
+
+    const fs::path empty_ext_dot{"/home/x/test."};
+    EXPECT_EQ(empty_ext_dot.filename(), fs::path{"test."});
+
+    const fs::path ext_only{"/tmp/.sh"};
+    EXPECT_EQ(ext_only.filename(), fs::path{".sh"});
+
+    const fs::path usual_justfile{"file.extension"};
+    EXPECT_EQ(usual_justfile.filename(), fs::path{"file.extension"});
+}
+
+TEST(pathTest, parent_path)
+{
+    const fs::path empty;
+    EXPECT_EQ(empty.parent_path(), empty);
+
+    const fs::path usual{"/home/x/test.txt"};
+    EXPECT_EQ(usual.parent_path(), fs::path{"/home/x"});
+
+    const fs::path empty_ext{"/home/x/test"};
+    EXPECT_EQ(empty_ext.parent_path(), fs::path{"/home/x"});
+
+    const fs::path dot{"."};
+    EXPECT_EQ(dot.parent_path(), empty);
+
+    const fs::path empty_ext_dot{"/home/x/test."};
+    EXPECT_EQ(empty_ext_dot.parent_path(), fs::path{"/home/x"});
+
+    const fs::path ext_only{"/tmp/.sh"};
+    EXPECT_EQ(ext_only.parent_path(), fs::path{"/tmp"});
+
+    const fs::path usual_justfile{"file.extension"};
+    EXPECT_EQ(usual_justfile.parent_path(), empty);
+
+    const fs::path root{"/"};
+    EXPECT_EQ(root.parent_path(), root);
+
+    const fs::path dot_abs{"/tmp/."};
+    EXPECT_EQ(dot_abs.parent_path(), fs::path{"/tmp"});
+
+    const fs::path dir{"/tmp/dir/"};
+    EXPECT_EQ(dir.parent_path(), fs::path{"/tmp/dir"});
+
+    const fs::path root_dir{"/tmp"};
+    EXPECT_EQ(root_dir.parent_path(), root);
+
+    const fs::path root_file{"/tmp.txt"};
+    EXPECT_EQ(root_file.parent_path(), root);
+}
+
+TEST(pathTest, replace_extension)
+{
+    const fs::path empty;
+    fs::path empty_cp{empty};
+    empty_cp.replace_extension(".txt");
+    EXPECT_EQ(empty_cp, fs::path{".txt"});
+
+    fs::path usual{"/home/x/test.txt"};
+    usual.replace_extension(".md");
+    EXPECT_EQ(usual, fs::path{"/home/x/test.md"});
+
+    fs::path empty_ext{"/home/x/test"};
+    empty_ext.replace_extension(".txt");
+    EXPECT_EQ(empty_ext, fs::path{"/home/x/test.txt"});
+
+    fs::path dot{"."};
+    dot.replace_extension(".txt");
+    EXPECT_EQ(dot, dot);
+
+    fs::path empty_ext_dot{"/home/x/test."};
+    empty_ext_dot.replace_extension();
+    EXPECT_EQ(empty_ext_dot, fs::path{"/home/x/test"});
+
+    fs::path ext_only{"/tmp/.hiddenfilename"};
+    ext_only.replace_extension(".extension");
+    EXPECT_EQ(ext_only, fs::path{"/tmp/.hiddenfilename.extension"});
+
+    fs::path usual_justfile{"file.replace_extension"};
+    usual_justfile.replace_extension();
+    EXPECT_EQ(usual_justfile, fs::path{"file"});
+}
+
+TEST(pathTest, eq)
+{
+    const fs::path lhs{"/tmp/foo/"};
+    const fs::path rhs{"/tmp/foo"};
+    EXPECT_TRUE(lhs != rhs);
+
+    const fs::path empty;
+    const fs::path dot{"."};
+    EXPECT_TRUE(empty != dot);
+}
+
+TEST(pathTest, concat)
+{
+    fs::path some{"/tmp/foo"};
+    some += "/bar";
+    EXPECT_EQ(some, fs::path{"/tmp/foo/bar"});
+
+    some += '/';
+    EXPECT_EQ(some, fs::path{"/tmp/foo/bar/"});
+
+    some += fs::path{"readme.md"};
+    EXPECT_EQ(some, fs::path{"/tmp/foo/bar/readme.md"});
+}
+
+TEST(pathTest, append)
+{
+    const fs::path some1{"/tmp/foo"};
+    const fs::path some2{"bar"};
+    fs::path some{some1 / some2};
+    EXPECT_EQ(some, fs::path{"/tmp/foo/bar"});
+    some /= fs::path{};
+    EXPECT_EQ(some, fs::path{"/tmp/foo/bar/"});
+    some /= fs::path{"readme.md"};
+    EXPECT_EQ(some, fs::path{"/tmp/foo/bar/readme.md"});
+
+    const fs::path empty;
+    fs::path current_some{empty};
+    current_some /= fs::path{"sub/file"};
+    EXPECT_EQ(current_some, fs::path{"sub/file"});
+
+    const fs::path dot{"."};
+    fs::path current_dot_some{dot};
+    current_dot_some /= fs::path{"sub/file"};
+    EXPECT_EQ(current_dot_some, fs::path{"./sub/file"});
+}
+
+class PhysicalFilesystemTest : public ::testing::Test
+{
+    void SetUp() override
+    {
+        fs::create_directories(physical_test_dir);
+    }
+
+    void TearDown() override
+    {
+        // don't risk deleting anything important accidentally, just keep the playground's base dir and content
+        TOO_NOOP;
+    }
+};
+
+TEST_F(PhysicalFilesystemTest, create_directories__single__exists_correcttype)
+{
+    const fs::path dir1 = physical_test_dir / fs::path{getSteadyUniqueNr() + "dir"};
+    const fs::path dir2 = physical_test_dir / fs::path{getSteadyUniqueNr() + "dir"};
+    EXPECT_TRUE(fs::create_directories(dir1));
+    EXPECT_FALSE(fs::create_directories(dir1));
+    EXPECT_TRUE(fs::create_directories(dir2));
+    EXPECT_FALSE(fs::create_directories(dir2));
+
+    EXPECT_TRUE(fs::exists(dir1));
+    EXPECT_TRUE(fs::exists(dir2));
+
+    EXPECT_TRUE(!fs::is_regular_file(dir1));
+    EXPECT_TRUE(!fs::is_regular_file(dir2));
+}
+
+TEST_F(PhysicalFilesystemTest, create_directories__recursive__exists_correcttype)
+{
+    const std::string subdirs_base = getSteadyUniqueNr() + "subdirs";
+
+    const fs::path subdirs{physical_test_dir / fs::path{subdirs_base} / fs::path{"d1/d2/d3"}};
+    EXPECT_TRUE(fs::create_directories(subdirs));
+    EXPECT_FALSE(fs::create_directories(subdirs));
+
+    EXPECT_TRUE(fs::exists(subdirs));
+
+    EXPECT_TRUE(!fs::is_regular_file(subdirs));
+}
+
+TEST_F(PhysicalFilesystemTest, exists__file_correcttype)
+{
+    const auto file = physical_test_dir / fs::path{"dummy" + getSteadyUniqueNr()};
+    EXPECT_TRUE(!fs::exists(file));
+    too::file::touch(file);
+    EXPECT_TRUE(fs::exists(file));
+    EXPECT_TRUE(fs::is_regular_file(file));
+}
+
+TEST_F(PhysicalFilesystemTest, remove__file_existence)
+{
+    const auto file = physical_test_dir / fs::path{"deadbeef" + getSteadyUniqueNr()};
+    ASSERT_TRUE(!fs::exists(file));
+    too::file::touch(file);
+    ASSERT_TRUE(fs::exists(file));
+    ASSERT_TRUE(fs::remove(file));
+    EXPECT_TRUE(!fs::exists(file));
+}
+
+TEST_F(PhysicalFilesystemTest, remove__dir_existence)
+{
+    const auto dir = physical_test_dir / fs::path{"deadbeef" + getSteadyUniqueNr()};
+    ASSERT_TRUE(!fs::exists(dir));
+    fs::create_directories(dir);
+    ASSERT_TRUE(fs::exists(dir));
+    ASSERT_TRUE(fs::remove(dir));
+    EXPECT_TRUE(!fs::exists(dir));
+}
+
+TEST_F(PhysicalFilesystemTest, current_path__get_set)
+{
+    std::error_code ec;
+    const auto orig = fs::current_path(ec);
+    ASSERT_FALSE(ec);
+    std::cout << orig.string() << "\n";
+    fs::current_path(physical_test_dir, ec);
+    ASSERT_FALSE(ec);
+    const auto chdir_back_to_orig = too::finally([&orig]()
+    {
+        std::error_code ec_;
+        fs::current_path(orig, ec_);
+        EXPECT_FALSE(ec_);
+    });
+    const auto changed_dir = fs::current_path(ec);
+    std::cout << changed_dir.string() << "\n";
+    std::cout << physical_test_dir.string() << "\n";
+    EXPECT_TRUE(changed_dir == physical_test_dir);
+    EXPECT_FALSE(ec);
+}
+
+TEST_F(PhysicalFilesystemTest, directory_iterator)
+{
+    const fs::path dir = physical_test_dir / fs::path{"some" + getSteadyUniqueNr()};
+    fs::create_directories(dir);
+
+    too::file::touch(dir / "f1");
+    fs::create_directories(dir / "sub");
+    too::file::touch(dir / "f2");
+    too::file::touch(dir / "f3");
+
+    fs::directory_iterator begin(dir);
+    fs::directory_iterator end;
+    std::vector<fs::directory_entry> entries(begin, end);
+    ASSERT_EQ(entries.size(), 4);
+    EXPECT_EQ(std::count(std::begin(entries), std::end(entries), dir / "f1"), 1);
+    EXPECT_EQ(std::count(std::begin(entries), std::end(entries), dir / "f2"), 1);
+    EXPECT_EQ(std::count(std::begin(entries), std::end(entries), dir / "f3"), 1);
+    EXPECT_EQ(std::count(std::begin(entries), std::end(entries), dir / "sub"), 1);
+}
