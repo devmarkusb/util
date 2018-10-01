4ea26fdd2d20f1f1b12d50c2b1ccd78501333816
MarkusB <markusb81@web.de>
2018-10-01 22:50:37 +0200

    msvc warning fix

--- buildenv/cmake_util/googletest_as_subdir.cmake
+++ buildenv/cmake_util/googletest_as_subdir.cmake
@@ -3,9 +3,9 @@
 # and gtest. This also seems to enable access to the include path.
 # If you don't need gmock, linking to gtest or gtest_main is sufficient.
 
-if(NOT DEFINED CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
+if (NOT DEFINED CMAKE_SUPPRESS_DEVELOPER_WARNINGS)
     set(CMAKE_SUPPRESS_DEVELOPER_WARNINGS 1 CACHE INTERNAL "No dev warnings")
-endif()
+endif ()
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 set(BUILD_GMOCK ON CACHE BOOL "" FORCE)
 set(BUILD_GTEST ON CACHE BOOL "" FORCE)
@@ -22,6 +22,11 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
             -Wno-zero-as-null-pointer-constant -Wno-sign-conversion -Wno-shift-sign-overflow
             -Wno-missing-variable-declarations -Wno-missing-field-initializers -Wno-used-but-marked-unused
             -Wno-disabled-macro-expansion -Wno-missing-prototypes)
-    target_compile_options(gmock PUBLIC ${TOO_gtest_compile_options})
-    target_compile_options(gtest PUBLIC ${TOO_gtest_compile_options})
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+    set(TOO_gtest_compile_options
+            /wd4389)
+else ()
+    set(TOO_gtest_compile_options)
 endif ()
+target_compile_options(gmock PUBLIC ${TOO_gtest_compile_options})
+target_compile_options(gtest PUBLIC ${TOO_gtest_compile_options})
