f72771b0c79632882e1d5f8de632092408ec991e
MarkusB <markusb81@web.de>
2019-02-02 22:30:14 +0100

    added format_number

---
+++ basiccodesupport/include/toolib/format_number.h
@@ -0,0 +1,51 @@
+// Markus Borris, 2019
+// This file is part of tfl library.
+
+//!
+/**
+ */
+//! \file
+
+#ifndef FORMAT_NUMBER_H_sdkihfx378gh87234xgh33h3
+#define FORMAT_NUMBER_H_sdkihfx378gh87234xgh33h3
+
+#include <locale>
+#include <sstream>
+#include <string>
+
+
+namespace too::fmt
+{
+namespace detail
+{
+class GroupThousands_numpunct : public std::numpunct<char>
+{
+public:
+    explicit GroupThousands_numpunct(char sep) : sep_{sep} {}
+
+protected:
+    char do_thousands_sep() const override
+    {
+        return sep_;
+    }
+
+    std::string do_grouping() const override
+    {
+        return "\03";
+    }
+
+private:
+    char sep_{};
+};
+} // detail
+
+template <typename T>
+inline std::string groupThousands(T&& t, char sep)
+{
+    std::stringstream ss;
+    ss.imbue(std::locale(std::locale::classic(), new detail::GroupThousands_numpunct{sep}));
+    ss << t;
+    return ss.str();
+}
+} // too::fmt
+#endif
