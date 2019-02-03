8a961c701e363d116783382499d5a39fcffb5940
MarkusB <markusb81@web.de>
2019-02-03 22:37:11 +0100

    added circular buffer

---
+++ basiccodesupport/include/toolib/std/circular_buffer.h
@@ -0,0 +1,208 @@
+// Markus Borris, 2019
+// This file is part of tfl library.
+
+//!
+/**
+ */
+//! \file
+
+#ifndef CIRCULAR_BUFFER_H_gfhcio489cghx4m9gh39u8hx3gh
+#define CIRCULAR_BUFFER_H_gfhcio489cghx4m9gh39u8hx3gh
+
+#include "toolib/assert.h"
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <vector>
+
+
+namespace too
+{
+namespace detail
+{
+template <typename T>
+class CircularBuffer_common_impl
+{
+protected:
+    template <typename T_, typename Buffer>
+    void push(T_&& item, Buffer&& buf, size_t capacity) noexcept
+    {
+        buf[head_] = std::forward<T_>(item);
+
+        if (full_)
+            tail_ = (tail_ + 1) % capacity;
+
+        head_ = (head_ + 1) % capacity;
+
+        full_ = head_ == tail_;
+    }
+
+    //! Don't use, not yet working correctly.
+    template <typename T_, typename Buffer>
+    void emplace(T_&& item, Buffer&& buf, size_t capacity) noexcept
+    {
+        auto it = std::begin(buf);
+        std::advance(it, head_);
+        buf.emplace(it, std::forward<T_>(item));
+
+        if (full_)
+            tail_ = (tail_ + 1) % capacity;
+
+        head_ = (head_ + 1) % capacity;
+
+        full_ = head_ == tail_;
+    }
+
+    template <typename Buffer>
+    bool try_pop(T& poppedItem, Buffer&& buf, size_t capacity) noexcept
+    {
+        if (empty())
+            return false;
+
+        poppedItem = buf[tail_];
+        full_      = false;
+        tail_      = (tail_ + 1) % capacity;
+
+        return true;
+    }
+
+    void reset() noexcept
+    {
+        head_ = tail_;
+        full_ = false;
+    }
+
+    bool empty() const noexcept
+    {
+        return (!full_ && (head_ == tail_));
+    }
+
+    bool full() const noexcept
+    {
+        return full_;
+    }
+
+    size_t size(size_t capacity) const noexcept
+    {
+        size_t size = capacity;
+
+        if (!full_)
+        {
+            if (head_ >= tail_)
+                size = head_ - tail_;
+            else
+                size = capacity + head_ - tail_;
+        }
+
+        return size;
+    }
+
+private:
+    size_t head_{0};
+    size_t tail_{0};
+    bool full_{};
+};
+} // detail
+
+template <typename T>
+class CircularBuffer : private detail::CircularBuffer_common_impl<T>
+{
+public:
+    using Base = detail::CircularBuffer_common_impl<T>;
+
+    //! Expects \param capacity > 0.
+    explicit CircularBuffer(size_t capacity) : capacity_(capacity)
+    {
+        TOO_EXPECT(capacity > 0);
+
+        buf_.resize(capacity);
+    }
+
+    template <typename T_>
+    void push(T_&& item) noexcept
+    {
+        Base::push(std::forward<T_>(item), buf_, capacity_);
+    }
+
+    //! Don't use, not yet working correctly.
+    template <typename T_>
+    void emplace(T_&& item) noexcept
+    {
+        Base::emplace(std::forward<T_>(item), buf_, capacity_);
+    }
+
+    bool try_pop(T& poppedItem) noexcept
+    {
+        return Base::try_pop(poppedItem, buf_, capacity_);
+    }
+
+    //void reset() noexcept
+    using Base::reset;
+
+    //bool empty() const noexcept
+    using Base::empty;
+
+    //bool full() const noexcept
+    using Base::full;
+
+    size_t capacity() const noexcept { return capacity_; }
+
+    size_t size() const noexcept
+    {
+        return Base::size(capacity_);
+    }
+
+private:
+    std::vector<T> buf_;
+    size_t capacity_{};
+};
+
+
+//! Expects \param capacity > 0. The "_static" means static allocation.
+template <typename T, size_t capacity_>
+class CircularBuffer_static : private detail::CircularBuffer_common_impl<T>
+{
+public:
+    static_assert(capacity_ > 0);
+
+    using Base = detail::CircularBuffer_common_impl<T>;
+
+    template <typename T_>
+    void push(T_&& item) noexcept
+    {
+        Base::push(std::forward<T_>(item), buf_, capacity_);
+    }
+
+    //! No difference to push here.
+    template <typename T_>
+    void emplace(T_&& item) noexcept
+    {
+        Base::push(std::forward<T_>(item), buf_, capacity_);
+    }
+
+    bool try_pop(T& poppedItem) noexcept
+    {
+        return Base::try_pop(poppedItem, buf_, capacity_);
+    }
+
+    //void reset() noexcept
+    using Base::reset;
+
+    //bool empty() const noexcept
+    using Base::empty;
+
+    //bool full() const noexcept
+    using Base::full;
+
+    size_t capacity() const noexcept { return capacity_; }
+
+    size_t size() const noexcept
+    {
+        return Base::size(capacity_);
+    }
+
+private:
+    std::array<T, capacity_> buf_;
+};
+} // too
+#endif
---
+++ basiccodesupport/include/toolib/std/circular_buffer.test.cpp
@@ -0,0 +1,541 @@
+#include "circular_buffer.h"
+#include "gtest/gtest.h"
+#include <string>
+
+
+TEST(CircularBuffer, construction)
+{
+    too::CircularBuffer<int> cb{3};
+    EXPECT_EQ(cb.capacity(), 3);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    int item{};
+    ASSERT_FALSE(cb.try_pop(item));
+    EXPECT_EQ(cb.capacity(), 3);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.reset();
+    EXPECT_EQ(cb.capacity(), 3);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, push)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.push(42);
+    EXPECT_EQ(cb.size(), 1);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, emplace)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.emplace(42);
+    EXPECT_EQ(cb.size(), 1);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, reset)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.push(42);
+    cb.reset();
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, pop)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.push(42);
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 42);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, pop_toomuch)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.push(42);
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 42);
+    ASSERT_FALSE(cb.try_pop(item));
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, push_until_full_and_pop)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 42);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, push_until_full_and_reset)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    cb.reset();
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, push_until_overflow_and_pop)
+{
+    too::CircularBuffer<int> cb{3};
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    cb.push(45);
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 43);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 44);
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 45);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, push_overflow_and_pop)
+{
+    too::CircularBuffer<std::string> cb{3};
+    cb.push("42");
+    cb.push("43");
+    cb.push("44");
+    cb.push("45");
+    cb.push("46");
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    std::string item;
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "44");
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "45");
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "46");
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+//TEST(CircularBuffer, emplace_overflow_and_pop)
+//{
+//    too::CircularBuffer<std::string> cb{3};
+//    cb.emplace("42");
+//    cb.emplace("43");
+//    cb.emplace("44");
+//    cb.emplace("45");
+//    cb.emplace("46");
+//    EXPECT_EQ(cb.size(), 3);
+//    EXPECT_TRUE(!cb.empty());
+//    EXPECT_TRUE(cb.full());
+//
+//    EXPECT_STREQ(cb.pop().c_str(), "44");
+//    EXPECT_TRUE(!cb.empty());
+//    EXPECT_TRUE(!cb.full());
+//
+//    EXPECT_STREQ(cb.pop().c_str(), "45");
+//    EXPECT_STREQ(cb.pop().c_str(), "46");
+//    EXPECT_EQ(cb.size(), 0);
+//    EXPECT_TRUE(cb.empty());
+//    EXPECT_TRUE(!cb.full());
+//}
+
+TEST(CircularBuffer, doublebuffer_maxsize_2)
+{
+    too::CircularBuffer<int> cb{2};
+    EXPECT_EQ(cb.capacity(), 2);
+    EXPECT_EQ(cb.size(), 0);
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    cb.push(45);
+    EXPECT_EQ(cb.size(), 2);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 44);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 45);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.push(46);
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 46);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, pathological_maxsize_1)
+{
+    too::CircularBuffer<int> cb{1};
+    EXPECT_EQ(cb.capacity(), 1);
+    EXPECT_EQ(cb.size(), 0);
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    cb.push(45);
+    EXPECT_EQ(cb.size(), 1);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 45);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_FALSE(cb.try_pop(item));
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.push(46);
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 46);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer, other_type)
+{
+    too::CircularBuffer<std::string> cb{2};
+    EXPECT_EQ(cb.capacity(), 2);
+    EXPECT_EQ(cb.size(), 0);
+    cb.push("s1");
+    cb.push("s2");
+    cb.push("s3");
+    cb.push("s4");
+    EXPECT_EQ(cb.size(), 2);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    std::string item;
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "s3");
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "s4");
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.push("s5");
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "s5");
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+
+//####################################################################################################################
+
+TEST(CircularBuffer_static, construction)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    EXPECT_EQ(cb.capacity(), 3);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    int item{};
+    ASSERT_FALSE(cb.try_pop(item));
+    EXPECT_EQ(cb.capacity(), 3);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.reset();
+    EXPECT_EQ(cb.capacity(), 3);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, push)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    cb.push(42);
+    EXPECT_EQ(cb.size(), 1);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, reset)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    cb.push(42);
+    cb.reset();
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, pop)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    cb.push(42);
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 42);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, pop_toomuch)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    cb.push(42);
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 42);
+    ASSERT_FALSE(cb.try_pop(item));
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, push_until_full_and_pop)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 42);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, push_until_full_and_reset)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    cb.reset();
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, push_until_overflow_and_pop)
+{
+    too::CircularBuffer_static<int, 3> cb;
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    cb.push(45);
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 43);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 44);
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 45);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, push_overflow_and_pop)
+{
+    too::CircularBuffer_static<std::string, 3> cb;
+    cb.push("42");
+    cb.push("43");
+    cb.push("44");
+    cb.push("45");
+    cb.push("46");
+    EXPECT_EQ(cb.size(), 3);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    std::string item;
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "44");
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "45");
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "46");
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, doubleBuffer_static_maxsize_2)
+{
+    too::CircularBuffer_static<int, 2> cb;
+    EXPECT_EQ(cb.capacity(), 2);
+    EXPECT_EQ(cb.size(), 0);
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    cb.push(45);
+    EXPECT_EQ(cb.size(), 2);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 44);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 45);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.push(46);
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 46);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, pathological_maxsize_1)
+{
+    too::CircularBuffer_static<int, 1> cb;
+    EXPECT_EQ(cb.capacity(), 1);
+    EXPECT_EQ(cb.size(), 0);
+    cb.push(42);
+    cb.push(43);
+    cb.push(44);
+    cb.push(45);
+    EXPECT_EQ(cb.size(), 1);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    int item{};
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 45);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_FALSE(cb.try_pop(item));
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.push(46);
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_EQ(item, 46);
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
+
+TEST(CircularBuffer_static, other_type)
+{
+    too::CircularBuffer_static<std::string, 2> cb;
+    EXPECT_EQ(cb.capacity(), 2);
+    EXPECT_EQ(cb.size(), 0);
+    cb.push("s1");
+    cb.push("s2");
+    cb.push("s3");
+    cb.push("s4");
+    EXPECT_EQ(cb.size(), 2);
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(cb.full());
+
+    std::string item;
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "s3");
+    EXPECT_TRUE(!cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "s4");
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+
+    cb.push("s5");
+    ASSERT_TRUE(cb.try_pop(item));
+    EXPECT_STREQ(item.c_str(), "s5");
+    EXPECT_EQ(cb.size(), 0);
+    EXPECT_TRUE(cb.empty());
+    EXPECT_TRUE(!cb.full());
+}
