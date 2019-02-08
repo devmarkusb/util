2230e7a5c1110fbd01f92473f848c12e38a9745f
MarkusB <markusb81@web.de>
2019-02-08 19:29:11 +0100

    add compile time accumulate for arrays

---
+++ basiccodesupport/include/toolib/compiletime/ct_array.h
@@ -0,0 +1,45 @@
+// Markus Borris, 2019
+// This file is part of tfl library.
+
+//!
+/**
+*/
+//! \file
+
+#ifndef CT_ARRAY_H_uixngt3h478gh389xgh3984g
+#define CT_ARRAY_H_uixngt3h478gh389xgh3984g
+
+#include <array>
+
+
+namespace too
+{
+namespace detail
+{
+template <typename T, std::size_t N, typename Container>
+constexpr T ct_accumulate(Container&& arr, T init_val)
+{
+    T sum{init_val};
+
+    for (size_t i = 0; i < N; ++i)
+        sum += arr[i];
+
+    return sum;
+}
+} // detail
+
+//! Compile-time equivalent of std::accumulate at least for array.
+template <typename T, std::size_t N>
+constexpr T ct_accumulate(const std::array<T, N>& arr, T init_val)
+{
+    return detail::ct_accumulate<T, N>(arr, init_val);
+}
+
+//! C-array version. Cf. other ct_accumulate.
+template <typename T, std::size_t N>
+constexpr T ct_accumulate(const T (&arr)[N], T init_val)
+{
+    return detail::ct_accumulate<T, N>(arr, init_val);
+}
+} // too
+#endif
