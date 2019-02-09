f6238ed7e47aa175a40f2590124d18cd43926514
MarkusB <markusb81@web.de>
2019-02-09 19:28:59 +0100

    add lotsd of really nice bit tools, especially a standard bit field replacement

--- basiccodesupport/include/toolib/bits.h
+++ basiccodesupport/include/toolib/bits.h
@@ -10,41 +10,142 @@
 #define BITS_H_sljkhvnx2hng7835ghm3g7h3
 
 #include "toolib/assert.h"
+#include "toolib/enum_cast.h"
+#include <array>
 #include <cstdint>
 #include <limits>
+#include <numeric>
 
 
 namespace too
 {
 namespace bits
 {
+using Idx = uint16_t;
+using Count = uint16_t;
+using Diff = int;
+
 //! \Returns number of bits of the arbitrary \param Type.
 template<typename Type>
-inline constexpr uint16_t num() noexcept
+constexpr Count count() noexcept
 {
     return 8 * sizeof(Type);
 }
 
-/** Produces, \returns, a bitmask with a connected range of 1's (and 0's out of that range) starting with the least
-    significant bit (from the right) given by the 0-based \param beginLSB up to one before the least significant bit
-    (from the right) given by the 0-based \param endLSB. To emphasize: we start counting bits with 0. And endLSB marks
-    one place after the last bit of the range, that one isn't set.
-    The maximum supported range goes up to the 63th bit.
-    Examples:
-     - range (x, x) wouldn't set any bit, since it is the empty range (effectively tries to fill from x to x-1, which is
-         an empty interval in our sense where beginLSB <= endLSB needs to hold)
-     - range (0, 1) would only set the 0th bit from the right, 0b1
-     - range (0, 2) would result in 0b11, the 0th and 1st ones
-     - range (1, 3) would result in 0b110, the 1st and 2nd ones
-     - range (4, 5) would result in 0b10000, the 4th one
-     - range (1, 5) would result in 0b11110*/
-inline uint64_t makeRangeOfOnes(uint16_t beginLSB, uint16_t endLSB) noexcept
+//! Makes a bit mask of \param count 1's, > 0 starting at 0-based index \param idx.
+/** \Param TargetType is typically chosen to be uintX_t, that is unsigned with some arbitrary bit count.
+    You can think of the index starting at the LSB (least significant bit, which comes last in the memory order
+    of big endian, but endianess doesn't matter considering the realm of this function alone).*/
+template <typename TargetType = uint64_t>
+constexpr TargetType makeMask(Idx idx, Count count) noexcept
 {
-    TOO_EXPECT(beginLSB <= endLSB);
-    TOO_EXPECT(beginLSB < num<uint64_t>());
-    TOO_EXPECT(endLSB <= num<uint64_t>());
+    static_assert(std::is_unsigned_v<TargetType>);
+    TOO_EXPECT(count > 0);
+    TOO_EXPECT(idx + count <= too::bits::count<TargetType>());
 
-    return ((uint64_t(1) << (endLSB - beginLSB)) - 1) << beginLSB;
+    return ((TargetType{1} << count) - 1) << idx;
+}
+
+//! Reads \param count > 0 bits of \param from starting at 0-based index \param idx (0 is LSB).
+template <typename SourceType = uint64_t>
+constexpr SourceType read(SourceType from, Idx idx, Count count) noexcept
+{
+    TOO_EXPECT(count > 0);
+    TOO_EXPECT(idx + count <= too::bits::count<SourceType>());
+
+    return (from & too::bits::makeMask<SourceType>(idx, count)) >> idx;
+}
+
+//! Like read. But here you can also opt for a different target type.
+/** As typically reading out a subset of bits results in a smaller type.*/
+template <typename TargetType, typename SourceType /*>*/= TargetType>
+constexpr TargetType readAndCast(SourceType from, Idx idx, Count count) noexcept
+{
+    TOO_EXPECT(count > 0);
+    TOO_EXPECT(idx + count <= too::bits::count<SourceType>());
+    TOO_EXPECT(count <= too::bits::count<TargetType>());
+
+    return (from & too::bits::makeMask<SourceType>(idx, count)) >> idx;
+}
+
+
+//! Write \param count > 0 bits of \param from into \param to starting at 0-based index \param idx there (0 is LSB).
+template <typename TargetType = uint64_t, typename SourceType = uint64_t>
+constexpr TargetType write(TargetType to, Idx idx, Count count, SourceType from) noexcept
+{
+    TOO_EXPECT(count > 0);
+    TOO_EXPECT(idx + count <= too::bits::count<TargetType>());
+    TOO_EXPECT(count <= too::bits::count<SourceType>());
+
+    return (to & (~too::bits::makeMask(idx, count))) | (from << idx);
+}
+
+//! A replacement for the suboptimal standard bitfields.
+/** Usage: Decide for a data type large enough to hold your bits, declare an enum (class) with names for your
+    distinct fields sharing the bits, with a last entry for simple calculation of the field count.
+    Then finally construct Fields with the desired counts of bits of each field.
+    enum Fields
+    {
+        first,
+        second,
+        third,
+        fields_end, // one behind last
+    }
+    too::bits::Field<uint32_t, Fields, fields_end> bits_{16, 8, 8};
+
+    void f()
+    {
+        bits_.set(Fields::second, 255);
+        assert(bits_.get(Fields::second) == 255);
+    }
+    */
+template <typename BitDataType, typename EnumType, size_t fields>
+class Fields
+{
+public:
+    static_assert(std::is_unsigned_v<BitDataType>);
+    static_assert(std::is_enum_v<EnumType>);
+    static_assert(fields > 0);
+
+    //! Needs exactly as many arguments as there are fields.
+    template <typename... Counts>
+    explicit constexpr Fields(Counts... counts) noexcept
+    {
+        static_assert(fields == sizeof...(counts));
+
+        counts_ = {static_cast<Count>(counts)...};
+        indices_[0] = {};
+        std::partial_sum(std::begin(counts_), std::end(counts_) - 1, std::begin(indices_) + 1);
+        // runtime check as long as we don't have a compiletime partial_sum
+        TOO_ENSURE(indices_.back() + counts_.back() <= count<BitDataType>());
+    }
+
+    template <typename SourceDataType>
+    constexpr void set(EnumType field, SourceDataType value) noexcept
+    {
+        const auto fieldnr{as_number(field)};
+        data_ = write<BitDataType, SourceDataType>(data_, indices_[fieldnr], counts_[fieldnr], value);
+    }
+
+    template <typename TargetDataType = BitDataType>
+    constexpr TargetDataType get(EnumType field) const noexcept
+    {
+        const auto fieldnr{as_number(field)};
+        return readAndCast<TargetDataType, BitDataType>(data_, indices_[fieldnr], counts_[fieldnr]);
+    }
+
+private:
+    BitDataType data_{};
+    std::array<Count, fields> counts_; // count of bits of each field
+    std::array<Count, fields> indices_; // indices where field sits
+};
+
+//! Evaluated at runtime, a compile-time implementation doesn't seem easy.
+/** Big endian means that the MSB (most significant bit) comes first in memory.*/
+inline bool is_bigendian() noexcept
+{
+    static const int one{1};
+    return *reinterpret_cast<const uint8_t*>(&one) == uint8_t{0};
 }
 } // bits
 } // too
--- basiccodesupport/include/toolib/bits.test.cpp
+++ basiccodesupport/include/toolib/bits.test.cpp
@@ -1,55 +1,300 @@
 #include "bits.h"
+#include "toolib/enum_cast.h"
 #include "gtest/gtest.h"
+#include <type_traits>
 
 
 TEST(bits_num, various)
 {
-    static_assert(too::bits::num<uint8_t>() == 8);
-    static_assert(too::bits::num<uint32_t>() == 32);
-    static_assert(too::bits::num<uint64_t>() == 64);
+    static_assert(too::bits::count<uint8_t>() == 8);
+    static_assert(too::bits::count<uint32_t>() == 32);
+    static_assert(too::bits::count<uint64_t>() == 64);
 
-    static_assert(too::bits::num<int8_t>() == 8);
-    static_assert(too::bits::num<int32_t>() == 32);
-    static_assert(too::bits::num<int64_t>() == 64);
+    static_assert(too::bits::count<int8_t>() == 8);
+    static_assert(too::bits::count<int32_t>() == 32);
+    static_assert(too::bits::count<int64_t>() == 64);
 }
 
-TEST(bits_makeRangeOfOnes, empty)
+TEST(bits_makeMask, empty)
 {
-    EXPECT_EQ(too::bits::makeRangeOfOnes(0, 0), 0u);
-    EXPECT_EQ(too::bits::makeRangeOfOnes(42, 42), 0u);
+    EXPECT_DEBUG_DEATH(too::bits::makeMask(5, 0), "ssert");
 }
 
-TEST(bits_makeRangeOfOnes, bitNr0)
+TEST(bits_makeMask, bitNr0)
 {
-    EXPECT_EQ(too::bits::makeRangeOfOnes(0, 1), 0b1u);
+    EXPECT_EQ(too::bits::makeMask(0, 1), 0b1u);
 }
 
-TEST(bits_makeRangeOfOnes, bitNr1)
+TEST(bits_makeMask, bitNr1)
 {
-    EXPECT_EQ(too::bits::makeRangeOfOnes(1, 2), 0b10u);
+    EXPECT_EQ(too::bits::makeMask(1, 1), 0b10u);
 }
 
-TEST(bits_makeRangeOfOnes, bitNr63)
+TEST(bits_makeMask, bitNr63)
 {
-    EXPECT_EQ(too::bits::makeRangeOfOnes(63, 64), uint64_t(1) << uint64_t(63));
+    EXPECT_EQ(too::bits::makeMask<uint64_t>(63, 1), uint64_t(1) << uint64_t(63));
 }
 
-TEST(bits_makeRangeOfOnes, highestTwoBits)
+TEST(bits_makeMask, highestTwoBits)
 {
-    EXPECT_EQ(too::bits::makeRangeOfOnes(62, 64), (uint64_t(1) << uint64_t(63)) | (uint64_t(1) << uint64_t(62)));
+    EXPECT_EQ(too::bits::makeMask<uint64_t>(62, 2), (uint64_t(1) << uint64_t(63)) | (uint64_t(1) << uint64_t(62)));
 }
 
-TEST(bits_makeRangeOfOnes, lowestTwoBits)
+TEST(bits_makeMask, lowestTwoBits)
 {
-    EXPECT_EQ(too::bits::makeRangeOfOnes(0, 2), 0b11u);
+    EXPECT_EQ(too::bits::makeMask(0, 2), 0b11u);
 }
 
-TEST(bits_makeRangeOfOnes, someMiddleRangeOfBits)
+TEST(bits_makeMask, someMiddleRangeOfBits)
 {
-    EXPECT_EQ(too::bits::makeRangeOfOnes(3, 9), 0b111111000u);
+    EXPECT_EQ(too::bits::makeMask(3, 6), 0b111111000u);
 }
 
-TEST(bits_makeRangeOfOnes, someMiddleRangeOfBits_castedToSufficientSmallerType)
+TEST(bits_makeMask, someMiddleRangeOfBits_castedToSufficientSmallerType)
 {
-    EXPECT_EQ(static_cast<uint32_t>(too::bits::makeRangeOfOnes(3, 9)), 0b111111000u);
+    EXPECT_EQ(too::bits::makeMask<uint8_t>(2, 6),   0b11111100u);
+    EXPECT_DEBUG_DEATH(too::bits::makeMask<uint8_t>(2, 7), "ssert");
+    EXPECT_EQ(too::bits::makeMask<uint16_t>(2, 7), 0b111111100u);
+}
+
+TEST(bits_read, typical)
+{
+    EXPECT_EQ(too::bits::read(0b11000101100u, 2, 5), 0b01011u);
+    const auto x = too::bits::readAndCast<uint8_t, uint16_t>(0b11000101100u, 2, 5);
+    EXPECT_EQ(x, uint8_t{0b01011});
+    EXPECT_DEBUG_DEATH(too::bits::makeMask<uint8_t>(2, 7), "ssert");
+}
+
+namespace
+{
+// workaround, because gtest understands comma between template parameters as comma for the gtest macro;
+// so we need a template with one instead of two parameters
+template<typename TargetType>
+constexpr TargetType read_from16_testhelper(uint16_t data, too::bits::Idx idx, too::bits::Count count) noexcept
+{
+    return too::bits::readAndCast<TargetType, uint16_t>(data, idx, count);
+}
+} // namespace
+
+TEST(bits_read, range_overflows)
+{
+    EXPECT_DEBUG_DEATH(too::bits::read<uint8_t>(0b10101100u, 2, 7), "ssert");
+    EXPECT_DEBUG_DEATH(too::bits::read<uint8_t>(0b10101100u, 9, 0), "ssert");
+    EXPECT_DEBUG_DEATH(too::bits::read<uint8_t>(0b10101100u, 0, 9), "ssert");
+    EXPECT_DEBUG_DEATH(read_from16_testhelper<uint8_t>(0b11000101100u, 2, 9), "ssert");
+}
+
+TEST(bits_read, none)
+{
+    EXPECT_DEBUG_DEATH(too::bits::read<uint8_t>(0b10101100u, 2, 0), "ssert");
+}
+
+TEST(bits_read, one)
+{
+    EXPECT_EQ(too::bits::read<uint8_t>(0b10101100u, 2, 1), 1);
+    EXPECT_EQ(too::bits::read<uint8_t>(0b10101000u, 2, 1), 0);
+}
+
+TEST(bits_read, last)
+{
+    EXPECT_EQ(too::bits::read<uint8_t>(0b10101100u, 7, 1), 1);
+    EXPECT_EQ(too::bits::read<uint8_t>(0b00101100u, 7, 1), 0);
+}
+
+TEST(bits_read, all)
+{
+    EXPECT_EQ(too::bits::read<uint8_t>(0b10101100u, 0, 8), 0b10101100u);
+}
+
+TEST(bits_read, types)
+{
+    auto x = too::bits::read<uint8_t>(0b10101100u, 2, 1);
+    static_assert(std::is_same_v<decltype(x), uint8_t>);
+    auto x2 = too::bits::read<uint16_t>(0b10101100u, 2, 1);
+    static_assert(std::is_same_v<decltype(x2), uint16_t>);
+}
+
+TEST(bits_read, types_cast)
+{
+    auto x = too::bits::readAndCast<uint8_t, uint16_t>(0b10101100u, 2, 1);
+    static_assert(std::is_same_v<decltype(x), uint8_t>);
+    auto x2 = too::bits::readAndCast<uint16_t, uint32_t>(0b10101100u, 2, 1);
+    static_assert(std::is_same_v<decltype(x2), uint16_t>);
+}
+
+TEST(bits_write, typical)
+{
+    EXPECT_EQ(too::bits::write(0b11000101100u, 2, 5, 0b01011u), 0b11000101100u);
+    EXPECT_EQ(too::bits::write(0b11000101100u, 2, 5, 0b11111u), 0b11001111100u);
+    EXPECT_EQ(too::bits::write(0b11000101100u, 2, 5, 0b00000u), 0b11000000000u);
+    EXPECT_EQ(too::bits::write(0b11000101100u, 2, 5, 0b10101u), 0b11001010100u);
+}
+
+TEST(bits_write, different_sizes)
+{
+    uint16_t x16{0b11000101100};
+    uint8_t x8{0b101001};
+    EXPECT_EQ(too::bits::write(x16, 0, 3, x8), 0b11000101001u);
+    EXPECT_EQ(too::bits::write(x8, 0, 3, x16), 0b101100u);
+}
+
+TEST(bits_write, none)
+{
+    EXPECT_DEBUG_DEATH(too::bits::write(0b10101100u, 2, 0, 1), "ssert");
+}
+
+TEST(bits_write, one)
+{
+    EXPECT_EQ(too::bits::write(0b10101100u, 2, 1, 1), 0b10101100u);
+    EXPECT_EQ(too::bits::write(0b10101100u, 2, 1, 0), 0b10101000u);
+}
+
+namespace
+{
+// workaround, because gtest understands comma between template parameters as comma for the gtest macro;
+// so we need a template with one instead of two parameters
+template <typename SourceType = uint8_t>
+inline constexpr uint8_t write_to8_testhelper(uint8_t to, too::bits::Idx idx, too::bits::Count count, SourceType from) noexcept
+{
+    return too::bits::write<uint8_t, SourceType>(to, idx, count, from);
+}
+} // namespace
+
+TEST(bits_write, range_overflows)
+{
+    EXPECT_DEBUG_DEATH(too::bits::write<uint16_t>(0, 0, 17, 0), "ssert");
+    auto eat = too::bits::write<uint16_t>(0, 0, 16, 0);
+    EXPECT_DEBUG_DEATH(write_to8_testhelper<uint8_t >(0, 0, 9, 0), "ssert");
+    eat = write_to8_testhelper<uint8_t >(0, 0, 8, 0);
+}
+
+TEST(bits_write, last)
+{
+    EXPECT_EQ(too::bits::write<uint8_t>(0b10101100u, 7, 1, 0), 0b00101100u);
+    EXPECT_EQ(too::bits::write<uint8_t>(0b10101100u, 7, 1, 1), 0b10101100u);
+    EXPECT_EQ(too::bits::write(0b10101100u, 2, 1, 0), 0b10101000u);
+    EXPECT_EQ(write_to8_testhelper<uint8_t>(0b10101100u, 7, 1, 0b1u), 0b10101100u);
+    EXPECT_EQ(write_to8_testhelper<uint8_t>(0b10101100u, 7, 1, 0b0u), 0b00101100u);
+}
+
+TEST(bits_write, all)
+{
+    EXPECT_EQ(write_to8_testhelper<uint8_t>(0b10101100u, 0, 8, 0b11111111u), 0b11111111u);
+    EXPECT_EQ(write_to8_testhelper<uint8_t>(0b10101100u, 0, 8, 0), 0);
+    EXPECT_EQ(write_to8_testhelper<uint8_t>(0b11111111u, 0, 8, 0b10101100u), 0b10101100u);
+    EXPECT_EQ(write_to8_testhelper<uint8_t>(0b00000000u, 0, 8, 0b10101100u), 0b10101100u);
+}
+
+TEST(bits_Field, basics)
+{
+    struct S
+    {
+        enum Field
+        {
+            octdigit,
+            toggle,
+            flags,
+            end, // one behind last
+        };
+        too::bits::Fields<uint8_t, Field, too::as_number(Field::end)> bits_{3, 1, 4};
+    } s;
+
+    EXPECT_EQ(s.bits_.get(S::Field::octdigit), 0);
+    s.bits_.set(S::Field::octdigit, 7);
+    EXPECT_EQ(s.bits_.get(S::Field::octdigit), 7);
+    EXPECT_EQ(s.bits_.get(S::Field::toggle), 0);
+    EXPECT_EQ(s.bits_.get(S::Field::flags), 0);
+    s.bits_.set(S::Field::octdigit, 0);
+    EXPECT_EQ(s.bits_.get(S::Field::octdigit), 0);
+
+    EXPECT_EQ(s.bits_.get(S::Field::toggle), 0);
+    s.bits_.set(S::Field::toggle, 0);
+    EXPECT_EQ(s.bits_.get(S::Field::toggle), 0);
+    s.bits_.set(S::Field::toggle, 1);
+    EXPECT_EQ(s.bits_.get(S::Field::octdigit), 0);
+    EXPECT_EQ(s.bits_.get(S::Field::toggle), 1);
+    EXPECT_EQ(s.bits_.get(S::Field::flags), 0);
+
+    EXPECT_EQ(s.bits_.get(S::Field::flags), 0);
+    s.bits_.set(S::Field::flags, 0b0001);
+    EXPECT_EQ(s.bits_.get(S::Field::flags), 0b0001);
+    EXPECT_EQ(s.bits_.get(S::Field::octdigit), 0);
+    EXPECT_EQ(s.bits_.get(S::Field::toggle), 1);
+    s.bits_.set(S::Field::flags, 0b0010);
+    EXPECT_EQ(s.bits_.get(S::Field::flags), 0b0010);
+    s.bits_.set(S::Field::flags, 0b0011);
+    EXPECT_EQ(s.bits_.get(S::Field::flags), 0b0011);
+    s.bits_.set(S::Field::flags, 0b1000);
+    EXPECT_EQ(s.bits_.get(S::Field::flags), 0b1000);
+}
+
+TEST(bits_Field, experiment_char)
+{
+    struct S
+    {
+        enum Field
+        {
+            character,
+            flags,
+            end, // one behind last
+        };
+        too::bits::Fields<uint16_t, Field, too::as_number(Field::end)> bits_{8, 8};
+    } s;
+
+    EXPECT_EQ(s.bits_.get(S::Field::character), 0);
+    s.bits_.set(S::Field::character, 'M');
+    EXPECT_EQ(s.bits_.get(S::Field::character), 'M');
+    s.bits_.set(S::Field::character, 255);
+    EXPECT_EQ(s.bits_.get(S::Field::character), 255);
+}
+
+TEST(bits_Field, one)
+{
+    struct S
+    {
+        enum Field
+        {
+            number,
+            end, // one behind last
+        };
+        too::bits::Fields<uint16_t, Field, too::as_number(Field::end)> bits_{16};
+    } s;
+
+    EXPECT_EQ(s.bits_.get(S::Field::number), 0);
+    s.bits_.set(S::Field::number, 1234);
+    EXPECT_EQ(s.bits_.get(S::Field::number), 1234);
+}
+
+TEST(bits_Field, some_capacity_left)
+{
+    struct S
+    {
+        enum Field
+        {
+            number,
+            end, // one behind last
+        };
+        too::bits::Fields<uint32_t, Field, too::as_number(Field::end)> bits_{20};
+    } s;
+
+    EXPECT_EQ(s.bits_.get(S::Field::number), 0);
+    s.bits_.set(S::Field::number, 1234);
+    EXPECT_EQ(s.bits_.get(S::Field::number), 1234);
+}
+
+TEST(bits_Field, overflow)
+{
+    auto lambda = []() {
+        struct S
+        {
+            enum Field
+            {
+                number,
+                numberLarger,
+                end, // one behind last
+            };
+            too::bits::Fields<uint32_t, Field, too::as_number(Field::end)> bits_{16, 17};
+        } s;
+    };
+    EXPECT_DEBUG_DEATH(lambda(), "ssert");
 }
