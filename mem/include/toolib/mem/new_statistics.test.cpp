67439014796c54a33f71916c3084f614382db6a5
MarkusB <markusb81@web.de>
2019-02-10 17:54:26 +0100

    new_statistics: add tests

--- mem/include/toolib/mem/new_statistics.test.cpp
+++ mem/include/toolib/mem/new_statistics.test.cpp
@@ -10,23 +10,42 @@
 int main()
 {
     std::cout << "Running main() from " << __FILE__ << "\n";
+
+    TOO_ASSERT_THROW(too::mem::Statistics::instance().newCalls() == 0);
+    TOO_ASSERT_THROW(too::mem::Statistics::instance().deleteCalls() == 0);
+    TOO_ASSERT_THROW(too::mem::Statistics::instance().peakSize() == 0);
+
     {
-        int *p1 = new int;
+        int* p1 = new int;
+        TOO_ASSERT_THROW(too::mem::Statistics::instance().newCalls() == 1);
+        TOO_ASSERT_THROW(too::mem::Statistics::instance().peakSize() == 4);
+
+        {
+            std::vector<int> l;
+            l.reserve(10);
+            TOO_ASSERT_THROW(too::mem::Statistics::instance().newCalls() == 2);
+            TOO_ASSERT_THROW(too::mem::Statistics::instance().peakSize() == 44);
+            for (int i = 0; i < 5; ++i)
+                l.push_back(i);
+        }
+        TOO_ASSERT_THROW(too::mem::Statistics::instance().deleteCalls() == 1);
+
         delete p1;
+        TOO_ASSERT_THROW(too::mem::Statistics::instance().deleteCalls() == 2);
 
+        TOO_ASSERT_THROW(too::mem::Statistics::instance().peakSize() == 44);
 
-        int *p2 = new int[10];
+        int* p2 = new int[10];
+        TOO_ASSERT_THROW(too::mem::Statistics::instance().newCalls() == 3);
         delete[] p2;
-
-        std::vector<int> l;
-        l.reserve(5);
-        for (int i = 0; i < 5; ++i)
-            l.push_back(i);
+        TOO_ASSERT_THROW(too::mem::Statistics::instance().deleteCalls() == 3);
     }
 
-    TOO_ASSERT_TERMINATE(too::mem::Statistics::instance().newCalls() == 3);
-    TOO_ASSERT_TERMINATE(too::mem::Statistics::instance().deleteCalls() == 3);
-    std::cout << "peak bytes: " << too::mem::Statistics::instance().peakSize() << "\n";
+    TOO_ASSERT_THROW(too::mem::Statistics::instance().newCalls() == 3);
+    TOO_ASSERT_THROW(too::mem::Statistics::instance().deleteCalls() == 3);
+    TOO_ASSERT_THROW(too::mem::Statistics::instance().peakSize() == 44);
+
+    std::cout << "PASSED (" << __FILE__ << ")\n";
 
     return too::prog_exit_success;
 }
