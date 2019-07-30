25282ff3acfe21d44b7b41a2e6583b5096eff2db
MarkusB <markusb81@web.de>
2019-07-30 22:57:30 +0200

    add most minimalistic random helper

---
+++ basiccodesupport/include/toolib/random.h
@@ -0,0 +1,27 @@
+// Markus Borris, 2019
+// This file is part of tfl library.
+
+//!
+/**
+*/
+//! \file
+
+#ifndef RANDOM_H_94xt4378j37843784n3
+#define RANDOM_H_94xt4378j37843784n3
+
+#include <random>
+
+
+namespace too::math
+{
+//! Generates a random number within half-open interval [0.0, 1.0).
+inline double random_frac()
+{
+    static std::random_device dev;
+    static std::mt19937 mt{dev()};
+    static std::uniform_real_distribution<double> dist{0.0, 1.0};
+    return dist(mt);
+}
+} // too::math
+
+#endif
