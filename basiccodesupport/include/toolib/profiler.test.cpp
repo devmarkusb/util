fe92408ebbff35eb6909ad66daf8c0b18979ac26
MarkusB <markusb81@web.de>
2019-08-27 19:25:26 +0200

    move minimalist profiler to tfl

---
+++ basiccodesupport/include/toolib/profiler.test.cpp
@@ -0,0 +1,15 @@
+#include "profiler.h"
+#include "gtest/gtest.h"
+#include <thread>
+
+
+TEST(profilerTest, basics)
+{
+    const auto before{too::profiler_now()};
+    std::this_thread::sleep_for(std::chrono::milliseconds(42));
+    const auto after{too::profiler_now()};
+    EXPECT_GT(after, before);
+    const auto diff{too::profiler_diff_s(before, after)};
+    EXPECT_GT(0.045, diff);
+    EXPECT_LT(0.039, diff);
+}
