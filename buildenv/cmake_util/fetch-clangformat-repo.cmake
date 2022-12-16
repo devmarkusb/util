9415103357420da1d484fb60f20b4fa0217b1e87
MarkusB <markusb81@web.de>
2022-12-16 17:42:37 +0100

    Revert "add mb-util cmake helper"
    This reverts commit d725ccfe9a78e719159a7cb88f025d6ad341f839.

--- buildenv/cmake_util/fetch-clangformat-repo.cmake
+++ buildenv/cmake_util/fetch-clangformat-repo.cmake
@@ -2,13 +2,13 @@ cmake_minimum_required(VERSION 3.14)
 
 include(FetchContent)
 
-FetchContent_Declare(mb-clangformat
+FetchContent_Declare(clangformat
         GIT_REPOSITORY "https://github.com/MarkusBorris/clangformat"
         GIT_TAG HEAD
         GIT_SHALLOW ON
         )
 
-FetchContent_MakeAvailable(mb-clangformat)
+FetchContent_MakeAvailable(clangformat)
 
 # of course the copied file is not intended to be edited
-file(COPY ${mb-clangformat_SOURCE_DIR}/.clang-format DESTINATION ${CMAKE_SOURCE_DIR}/)
+file(COPY ${clangformat_SOURCE_DIR}/.clang-format DESTINATION ${CMAKE_SOURCE_DIR}/)
