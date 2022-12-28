30e0a7576e23414b83015e2b3f21781a729d1e48
MarkusB <markusb81@web.de>
2022-12-29 00:54:42 +0100

    add easyloggingpp cmake util

---
+++ buildenv/cmake_util/easyloggingpp.cmake
@@ -0,0 +1,25 @@
+FetchContent_Declare(
+     easyloggingpp
+     GIT_REPOSITORY https://github.com/MarkusBorris/easyloggingpp
+     GIT_TAG        v9.96.7
+)
+
+FetchContent_MakeAvailable(easyloggingpp)
+
+set(easyloggingpp_INCLUDE_DIRS
+        ${easyloggingpp_SOURCE_DIR}/src
+        CACHE INTERNAL "easyloggingpp: include directories" FORCE)
+
+set(easyloggingpp_SOURCE_FILES
+        ${easyloggingpp_SOURCE_DIR}/src/easylogging++.cc
+        CACHE INTERNAL "easyloggingpp: source files" FORCE)
+
+if (MSVC)
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    string(CONCAT compileOptions
+            "-Wno-sign-conversion;-Wno-implicit-int-float-conversion;-Wno-implicit-int-conversion;"
+            "-Wno-range-loop-construct;-Wno-unused-parameter")
+    set_source_files_properties(${easyloggingpp_SOURCE_FILES} DIRECTORY ${CMAKE_SOURCE_DIR} PROPERTIES
+            COMPILE_OPTIONS "${compileOptions}")
+elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+endif ()
