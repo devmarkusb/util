85d0cf2374b85354550228ff34c3eb3ae90e9afe
MarkusB <markusb81@web.de>
2023-01-03 04:22:05 +0100

    qt-android-cmake version hard-wired

--- buildenv/cmake_util/qt-android-cmake.cmake
+++ buildenv/cmake_util/qt-android-cmake.cmake
@@ -1,8 +1,7 @@
 FetchContent_Declare(
      QtAndroidCMake
      GIT_REPOSITORY https://github.com/LaurentGomila/qt-android-cmake
-     GIT_TAG        origin/HEAD
+     GIT_TAG        fa363d60e389afbcd56c31ab4dd9723708b79fbe
 )
 
 FetchContent_MakeAvailable(QtAndroidCMake)
-message(QtAndroidCMake_SOURCE_DIR: ${QtAndroidCMake_SOURCE_DIR})
