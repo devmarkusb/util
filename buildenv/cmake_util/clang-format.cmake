eca3085b8c34ae3eaa4aa74c69f1eef30f210d6f
MarkusB <>
2023-01-07 11:52:01 +0100

    simplify clang-format cmake

--- buildenv/cmake_util/clang-format.cmake
+++ buildenv/cmake_util/clang-format.cmake
@@ -1,4 +1,20 @@
-# include in your project root once; generates a 'clang-format' target
+# Include in your project root(s) once. Generates 'clang-format' targets and more, see clangformat/README.md
+cmake_minimum_required(VERSION 3.14)
+
+include(FetchContent)
+
+FetchContent_Declare(mb-clangformat
+        GIT_REPOSITORY "https://github.com/devmarkusb/clangformat"
+        GIT_TAG origin/HEAD
+        GIT_SHALLOW ON
+        )
+
+FetchContent_MakeAvailable(mb-clangformat)
+
+# of course the copied files are not intended to be edited
+file(COPY ${mb-clangformat_SOURCE_DIR}/.clang-format DESTINATION ${PROJECT_SOURCE_DIR}/)
+file(COPY ${mb-clangformat_SOURCE_DIR}/clangformat.sh DESTINATION ${PROJECT_SOURCE_DIR}/
+        FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
 
 set(cxx_dirs_general "apps;include;libs;sdks;source;src;test")
 if (${PROJECT_NAME} STREQUAL "util")
