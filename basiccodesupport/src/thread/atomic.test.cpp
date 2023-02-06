05716dfd552388e940de336977e6ee0a68c98c54
MarkusB <>
2023-02-06 21:28:44 +0100

    lint

--- basiccodesupport/src/thread/atomic.test.cpp
+++ basiccodesupport/src/thread/atomic.test.cpp
@@ -4,23 +4,28 @@
 
 namespace ul = mb::ul;
 
+namespace
+{
+constexpr auto someNumberChoice{42};
+} // namespace
+
 TEST(atomic_updateMaximum, update)
 {
-    std::atomic<int> m{42};
-    ul::thread::atomic::updateMaximum(m, 43);
-    EXPECT_EQ(m.load(), 43);
+    std::atomic<int> m{someNumberChoice};
+    ul::thread::atomic::updateMaximum(m, someNumberChoice + 1);
+    EXPECT_EQ(m.load(), someNumberChoice + 1);
 }
 
 TEST(atomic_updateMaximum, no_update_eq)
 {
-    std::atomic<int> m{42};
-    ul::thread::atomic::updateMaximum(m, 42);
-    EXPECT_EQ(m.load(), 42);
+    std::atomic<int> m{someNumberChoice};
+    ul::thread::atomic::updateMaximum(m, someNumberChoice);
+    EXPECT_EQ(m.load(), someNumberChoice);
 }
 
 TEST(atomic_updateMaximum, no_update_less)
 {
-    std::atomic<int> m{42};
-    ul::thread::atomic::updateMaximum(m, 41);
-    EXPECT_EQ(m.load(), 42);
+    std::atomic<int> m{someNumberChoice};
+    ul::thread::atomic::updateMaximum(m, someNumberChoice - 1);
+    EXPECT_EQ(m.load(), someNumberChoice);
 }
