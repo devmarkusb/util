d29674c320fd282b1a6c1c74c994aad93b317254
MarkusB <>
2023-02-07 08:43:48 +0100

    cppcheck switchable, green
    Not yet working: dir exclusion

--- buildenv/cmake_util/cppcheck.cmake
+++ buildenv/cmake_util/cppcheck.cmake
@@ -1,16 +1,20 @@
-# include in your project root once; use cmake parameter -DCMAKE_CXX_CPPCHECK:FILEPATH=cppcheck
+# include in your project root once; switch on UL_CPPCHECK cmake option on demand
 
 cmake_minimum_required(VERSION 3.10)
 
-find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
-if (CMAKE_CXX_CPPCHECK)
-    list(
-        APPEND CMAKE_CXX_CPPCHECK
-        "--enable=warning"
-        "--inconclusive"
-        "--force"
-        "--inline-suppr"
-        "--suppressions-list=${CMAKE_PROJECT_DIR}/CppCheckSuppressions.txt"
-    )
-    message(CMAKE_CXX_CPPCHECK: ${CMAKE_CXX_CPPCHECK})
+option(UL_CPPCHECK "run cppcheck" OFF)
+
+if (UL_CPPCHECK)
+    find_program(CMAKE_CXX_CPPCHECK NAMES cppcheck)
+    if (CMAKE_CXX_CPPCHECK)
+        list(
+            APPEND CMAKE_CXX_CPPCHECK
+            "--enable=warning"
+            "--inconclusive"
+            "--force"
+            "--inline-suppr"
+            "--suppressions-list=${PROJECT_SOURCE_DIR}/CppCheckSuppressions.txt"
+        )
+        message(CMAKE_CXX_CPPCHECK: ${CMAKE_CXX_CPPCHECK})
+    endif()
 endif()
