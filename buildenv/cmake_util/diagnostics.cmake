a76cc1d8c00ebc900c8e6e9621a393b73f485e43
MarkusB <>
2023-02-08 19:59:55 +0100

    wip coverage

--- buildenv/cmake_util/diagnostics.cmake
+++ buildenv/cmake_util/diagnostics.cmake
@@ -8,6 +8,8 @@ set(c_compile_options)
 option(UL_ALL_WARNINGS "all possible warnings, switch on and adapt details if you want to see \
 a maximum of warnings" OFF)
 
+option(UL_COVERAGE "coverage" OFF)
+
 # sanitizers, mutually exclusive for now
 option(UL_ADDRESS_SAN "address sanitizer (asan)" OFF)
 option(UL_THREAD_SAN "thread sanitizer" OFF)
@@ -20,8 +22,8 @@ if (UL_ADDRESS_SAN)
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
         # nothing yet
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(cpp_compile_options -fsanitize=address)
-        set(cpp_link_options -fsanitize=address)
+        set(cpp_compile_options ${cpp_compile_options} -fsanitize=address)
+        set(cpp_link_options ${cpp_link_options}-fsanitize=address)
     endif ()
 elseif (UL_THREAD_SAN)
     if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
@@ -29,8 +31,8 @@ elseif (UL_THREAD_SAN)
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
         # nothing yet
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(cpp_compile_options -fsanitize=thread)
-        set(cpp_link_options -fsanitize=thread)
+        set(cpp_compile_options ${cpp_compile_options} -fsanitize=thread)
+        set(cpp_link_options ${cpp_link_options} -fsanitize=thread)
     endif ()
 elseif (UL_MEMORY_SAN)
     if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
@@ -38,8 +40,8 @@ elseif (UL_MEMORY_SAN)
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
         # nothing yet
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(cpp_compile_options -fsanitize=memory)
-        set(cpp_link_options -fsanitize=memory)
+        set(cpp_compile_options ${cpp_compile_options} -fsanitize=memory)
+        set(cpp_link_options ${cpp_link_options} -fsanitize=memory)
     endif ()
 elseif (UL_UNDEF_SAN)
     if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
@@ -47,8 +49,8 @@ elseif (UL_UNDEF_SAN)
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
         # nothing yet
     elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(cpp_compile_options -fsanitize=undefined)
-        set(cpp_link_options -fsanitize=undefined)
+        set(cpp_compile_options ${cpp_compile_options} -fsanitize=undefined)
+        set(cpp_link_options ${cpp_link_options} -fsanitize=undefined)
     endif ()
 endif ()
 
@@ -83,6 +85,15 @@ if (UL_ALL_WARNINGS)
     endif ()
 endif ()
 
+if (UL_COVERAGE)
+    set(cpp_compile_options ${cpp_compile_options})
+    set(cpp_link_options ${cpp_link_options})
+    if (NOT MSVC)
+        if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+            include(${CMAKE_CURRENT_LIST_DIR}/3rdparty/bilke/cmake-modules/CodeCoverage.cmake)
+        endif ()
+    endif ()
+endif()
 
 ### finally setting compile options ###
 
