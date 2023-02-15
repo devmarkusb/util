fe03e0af3f59ae091253684c0fdbb5efec4460a5
MarkusB <>
2023-02-15 12:26:31 +0100

    openmp fix

--- buildenv/cmake_util/openmp.cmake
+++ buildenv/cmake_util/openmp.cmake
@@ -1,17 +1,8 @@
-# file to include for OpenMP settings
+# File to include for OpenMP settings.
+# You should just need to link to OpenMP::OpenMP_CXX.
 
-set(cpp_compile_options)
-set(c_compile_options)
+find_package(OpenMP)
 
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    set(cpp_compile_options
-            -fopenmp)
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    set(cpp_compile_options
-            -fopenmp)
-endif ()
-
-add_compile_options(
-        "$<$<COMPILE_LANGUAGE:C>:${c_compile_options}>"
-        "$<$<COMPILE_LANGUAGE:CXX>:${cpp_compile_options}>")
+if (NOT OpenMP_CXX_FOUND)
+    message(FATAL_ERROR "Could not find OpenMP")
+endif()
