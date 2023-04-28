bf8e4e18682f1969f62e5225b312a44499093e44
MarkusB <>
2023-04-28 20:56:12 +0200

    clangformat additional dirs

--- buildenv/cmake_util/clang-format.cmake
+++ buildenv/cmake_util/clang-format.cmake
@@ -1,6 +1,8 @@
 # Include in your project root(s) once. Generates 'clang-format' targets and more, see clangformat/README.md
 cmake_minimum_required(VERSION 3.14)
 
+set(UL_ADDITIONAL_CLANG_FORMAT_DIRS "" CACHE STRING "provide ;-separated list in quotes as a whole")
+
 include(FetchContent)
 
 FetchContent_Declare(mb-clangformat
@@ -22,5 +24,6 @@ if (${PROJECT_NAME} STREQUAL "util")
 endif ()
 
 string(CONCAT cxx_dirs "${cxx_dirs_general}" "${cxx_dirs_util}")
+string(CONCAT cxx_dirs "${cxx_dirs}" "${UL_ADDITIONAL_CLANG_FORMAT_DIRS}")
 
 add_clang_format_project_target("${cxx_dirs}")
