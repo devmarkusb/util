cc690646ac9c6db1c3465f91d0005e4ecc13696c
MarkusB <>
2023-05-19 20:57:46 +0200

    clang-tidy rework

--- buildenv/cmake_util/clang-tidy.cmake
+++ buildenv/cmake_util/clang-tidy.cmake
@@ -1,28 +1,33 @@
-# Include in your project root once and before any add_subdirectory calls (to get your hands on everything
-# starting from there). Then configure UL_CLANGTIDY=ON.
+# Include in your project root(s) once. Generates 'clang-tidy' targets.
+# Before that, optionally define
+# - UL_ADDITIONAL_CLANG_TIDY_DIRS with a quoted ;-separated list
+# - UL_USE_DEFAULT_CLANG_TIDY_CONFIG ON if you want to overwrite the .clang-tidy file in your project dir by this
+#   repos' default.
+cmake_minimum_required(VERSION 3.14)
 
-option(UL_CLANGTIDY "Run build with clang-tidy. A .clang_tidy template is copied to your project root \
-if not there yet. Feel free to put it under version control, adapt and use with more sophisticated tools." OFF)
+include(FetchContent)
 
-if (NOT UL_CLANGTIDY)
-    return()
+FetchContent_Declare(mb-clangtidy
+    GIT_REPOSITORY "https://github.com/devmarkusb/clangtidy"
+    GIT_TAG origin/HEAD
+    GIT_SHALLOW ON
+    )
+
+FetchContent_MakeAvailable(mb-clangtidy)
+
+if (UL_USE_DEFAULT_CLANG_TIDY_CONFIG)
+    # of course the copied files are not intended to be edited
+    file(COPY ${mb-clangtidy_SOURCE_DIR}/.clang-tidy DESTINATION ${PROJECT_SOURCE_DIR}/)
 endif()
 
-set(projsrc ${PROJECT_SOURCE_DIR})
-set(currlst ${CMAKE_CURRENT_LIST_DIR})
-if (NOT EXISTS ${projsrc}/.clang_tidy)
-    file(COPY ${currlst}/clangtidy/.clang-tidy DESTINATION ${projsrc}/)
+set(cxx_dirs_general "apps;include;libs;sdks;source;src;test")
+if (${PROJECT_NAME} STREQUAL "util")
+    set(cxx_dirs_util ";basiccodesupport;buildenv;linklib;mem;string")
+endif ()
+
+string(CONCAT cxx_dirs "${cxx_dirs_general}" "${cxx_dirs_util}")
+if (UL_ADDITIONAL_CLANG_TIDY_DIRS)
+    string(CONCAT cxx_dirs "${cxx_dirs}" ";${UL_ADDITIONAL_CLANG_TIDY_DIRS}")
 endif()
 
-# just a preferred version
-set(CLANGTIDY_VER 15)
-
-find_program(CLANGTIDY_EXE NAMES clang-tidy-${CLANGTIDY_VER} clang-tidy)
-
-# For all the options see .clang-tidy file. There you can also specify something like
-# HeaderFilter: "^.*(?:_deps).*"gm
-# which seems to work there but not here.
-if (CLANGTIDY_EXE)
-    set(CMAKE_CXX_CLANG_TIDY
-            ${CLANGTIDY_EXE};)
-endif()
+add_clang_tidy_project_target("${cxx_dirs}")
