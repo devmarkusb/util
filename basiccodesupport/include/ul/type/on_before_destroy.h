db78c22262e7b9c692bcd980e6d5677f349008e2
MarkusB <>
2023-05-23 23:06:54 +0200

    tidy

--- basiccodesupport/include/ul/type/on_before_destroy.h
+++ basiccodesupport/include/ul/type/on_before_destroy.h
@@ -13,12 +13,12 @@ namespace mb::ul {
     object (the one that contains the injected object), you just need to make
     the containing object listen to the injected object notifying about its own
     destructtion. That is you need to
-        1 call onBeforeDestroy within the destructor of the injected object
+        1 call on_before_destroy within the destructor of the injected object
         2 within the containing object make sure that you
             2.2 register the containing object as listener within the injected object
                 right after/at injection
             2.1 set the pointer to the injected object to nullptr as soon as you get
-                called via onBeforeDestroy (implement onBeforeDestroy that way).
+                called via on_before_destroy (implement on_before_destroy that way).
 */
 struct OnBeforeDestroyListener : public Listener {
     ~OnBeforeDestroyListener() override = default;
--- basiccodesupport/src/ptr2ref.test.cpp
+++ basiccodesupport/src/ptr2ref.test.cpp
@@ -7,8 +7,8 @@ TEST(ptr2refTest, test) {
     constexpr auto some_nr{5};
     int i = some_nr;
     int* pi = &i;
-    int& refi = ul::ptr2_ref(pi);
+    int& refi = ul::ptr2ref(pi);
     EXPECT_TRUE(&refi == pi);
-    const int& crefi = ul::ptr2_ref(pi);
+    const int& crefi = ul::ptr2ref(pi);
     EXPECT_TRUE(&crefi == pi);
 }
