c2bcb8a390145a2b2ef02ede198e8c2f480b8adb
MarkusB <>
2023-07-21 20:24:34 +0200

    fix githooks for submodules

--- buildenv/cmake_util/githooks.cmake
+++ buildenv/cmake_util/githooks.cmake
@@ -1,4 +1,5 @@
 # include in your project root once; enables dir .githooks in project root as a place for git hooks under version
 # control, opposed to .git/hooks
 
-execute_process(COMMAND git config core.hookspath .githooks)
+# in order to work for submodules also I found no other way than to use --global
+execute_process(COMMAND git config --global core.hookspath .githooks)
