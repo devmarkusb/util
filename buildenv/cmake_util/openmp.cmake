7d1e52a27c8098d7601d43de0be67190bc4a41aa
MarkusB <>
2023-08-03 22:25:27 +0200

    fix clang-16

--- buildenv/cmake_util/openmp.cmake
+++ buildenv/cmake_util/openmp.cmake
@@ -4,5 +4,7 @@
 find_package(OpenMP)
 
 if (NOT OpenMP_CXX_FOUND)
-    message(FATAL_ERROR "Could not find OpenMP")
+    message(FATAL_ERROR "Could not find OpenMP. Hint: Interestingly it seems only a single version of "
+        "libomp-<ver>-dev can be installed at a time. So just install `$ sudo apt install libomp-15-dev` e.g., "
+        "and this might also automatically remove version 16 e.g.")
 endif()
