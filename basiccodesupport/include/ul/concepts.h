ee4168cb2b23448c0e07b949745f286a7a5ee690
MarkusB <>
2023-08-30 14:21:33 +0200

    +concepts

---
+++ basiccodesupport/include/ul/concepts.h
@@ -0,0 +1,29 @@
+/** \file
+Most general ones. Otherwise they are spread to where to belong thematically, e.g. math.
+*/
+
+#ifndef CONCEPTS_H_CFDDF4FE5F6D48D0BD50FCFBBAAE968A
+#define CONCEPTS_H_CFDDF4FE5F6D48D0BD50FCFBBAAE968A
+
+#include <concepts>
+
+namespace mb::ul {
+template <typename T>
+concept Dereferenceable = requires(T x) { *x; };
+
+template <typename T>
+concept Container = requires(T x) {
+                        { x.begin() } -> Dereferenceable;
+                        { x.end() } -> Dereferenceable;
+                        { x.size() } -> std::integral;
+                    };
+
+template <class T, class... U>
+concept AnyOf = (std::same_as<T, U> || ...);
+
+// not a concept, but no better place
+template <class>
+inline constexpr bool always_false_v = false;
+} // namespace mb::ul
+
+#endif
