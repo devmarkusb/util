9d3fc48b1006c27b25ce255cdc7eaa47b8481f9d
MarkusB <>
2023-09-27 06:13:58 +0200

    disable concepts for old compilers
    Needed for Android build.

--- basiccodesupport/include/ul/concepts.h
+++ basiccodesupport/include/ul/concepts.h
@@ -6,17 +6,27 @@ Most general ones. Otherwise they are spread to where to belong thematically, e.
 #define CONCEPTS_H_CFDDF4FE5F6D48D0BD50FCFBBAAE968A
 
 #include "ul/config.h"
+#if __has_include(<concepts>)
 #include <concepts>
+#endif
 
-namespace mb::ul {
+#if __cpp_concepts && __cpp_lib_concepts
+namespace mb::ul
+{
 template <typename T>
 concept Dereferenceable = requires(T x) { *x; };
 
 template <typename T>
 concept Container = requires(T x) {
-                        { x.begin() } -> Dereferenceable;
-                        { x.end() } -> Dereferenceable;
-                        { x.size() } -> std::integral;
+                        {
+                            x.begin()
+                            } -> Dereferenceable;
+                        {
+                            x.end()
+                            } -> Dereferenceable;
+                        {
+                            x.size()
+                            } -> std::integral;
                     };
 
 template <class T, class... U>
@@ -26,6 +36,7 @@ concept AnyOf = (std::same_as<T, U> || ...);
 template <class>
 inline constexpr bool always_false_v = false;
 } // namespace mb::ul
+#endif
 
 UL_HEADER_END
 
