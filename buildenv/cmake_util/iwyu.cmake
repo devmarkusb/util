b83766a49e295281a18d0fb94f2c3487fb037033
MarkusB <>
2023-10-09 23:35:10 +0200

    + iwyu (wip, not yet working)

---
+++ buildenv/cmake_util/iwyu.cmake
@@ -0,0 +1,7 @@
+# Not yet working, don't know why.
+# Usage:
+# set_property(TARGET target PROPERTY CXX_INCLUDE_WHAT_YOU_USE ${iwyu_path})
+
+cmake_minimum_required(VERSION 3.3 FATAL_ERROR)
+
+find_program(iwyu_path NAMES include-what-you-use iwyu REQUIRED)
