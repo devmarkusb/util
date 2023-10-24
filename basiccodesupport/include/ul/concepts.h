89d77a8e65778bab8ef908136c1b9a530793d085
MarkusB <>
2023-10-24 22:44:03 +0200

    concepts with bwds compatibility
    A prototype example.

--- basiccodesupport/include/ul/concepts.h
+++ basiccodesupport/include/ul/concepts.h
@@ -9,6 +9,7 @@ Most general ones. Otherwise they are spread to where to belong thematically, e.
 #if __has_include(<concepts>)
 #include <concepts>
 #endif
+#include <type_traits>
 
 #if __cpp_concepts && __cpp_lib_concepts
 namespace mb::ul {
@@ -22,13 +23,23 @@ concept Container = requires(T x) {
                         { x.size() } -> std::integral;
                     };
 
-template <class T, class... U>
+template <typename T, typename... U>
 concept AnyOf = (std::same_as<T, U> || ...);
 
+template <typename T>
+concept Arithmetic = std::is_arithmetic_v<T>;
+
 // not a concept, but no better place
-template <class>
+template <typename>
 inline constexpr bool always_false_v = false;
 } // namespace mb::ul
+
+/** Using concepts while unfortunately being forced to keep compatibility with older compilers:
+    Always prefer this instead of the requires clause - and prefer the requires clause in itself only, as it is not
+    possible to define a macro for typename replacements, at least not for namespace scoped concepts.*/
+#define UL_REQUIRES(x) requires x
+#else
+#define UL_REQUIRES(x)
 #endif
 
 UL_HEADER_END
--- basiccodesupport/include/ul/overflow.h
+++ basiccodesupport/include/ul/overflow.h
@@ -1,11 +1,13 @@
 #ifndef OVERFLOW_H_423509491CC54DFE8E60F36007D88CE6
 #define OVERFLOW_H_423509491CC54DFE8E60F36007D88CE6
 
+#include "ul/concepts.h"
 #include "ul/config.h"
 #include <limits>
 
 namespace mb::ul::math {
 template <typename N>
+UL_REQUIRES(Arithmetic<N>)
 bool mul_overflow(N a, N b) {
     if (a == -1 && b == std::numeric_limits<N>::min())
         return true;
