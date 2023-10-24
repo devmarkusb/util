0f885878d59af4ec863e8ca87f27c76a586d4ff8
MarkusB <>
2023-10-24 21:49:11 +0200

    compile fixes

--- buildenv/cmake_util/defaults.cmake
+++ buildenv/cmake_util/defaults.cmake
@@ -91,12 +91,13 @@ endif ()
 ### warnings ###
 
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Werror")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Werror \
+-Wno-deprecated-declarations -Wno-comment")
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wconversion -Werror \
--Wmissing-prototypes -Wno-c++11-narrowing -Wdocumentation")
+-Wmissing-prototypes -Wno-c++11-narrowing -Wdocumentation -Wno-deprecated-declarations")
 endif ()
 
 
