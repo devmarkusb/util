e6d67fe9a2e74e22a3685496dac59c4c1b9b2dd5
MarkusB <>
2023-11-10 13:38:52 +0100

    add to transformation

--- basiccodesupport/include/ul/transformation.h
+++ basiccodesupport/include/ul/transformation.h
@@ -38,7 +38,7 @@ DistanceType<F> distance(Domain<F> x, Domain<F> y, F f) {
 }
 
 template <Transformation F>
-Domain<F> collision_point_nonterminating_orbit(const Domain<F>& x, F f) {
+Domain<F> collision_point_nonterminating_orbit(Domain<F> x, F f) {
     auto slow = x;
     auto fast = f(x);
 
@@ -61,16 +61,31 @@ Domain<F> convergent_point(Domain<F> x0, Domain<F> x1, F f) {
 }
 
 template <Transformation F>
-Domain<F> connection_point_nonterminating_orbit(const Domain<F>& x, F f) {
+Domain<F> connection_point_nonterminating_orbit(Domain<F> x, F f) {
     return convergent_point(x, f(collision_point_nonterminating_orbit(x, f)), f);
+}
+
+/** \return (0, c-1, x) for circular orbits and
+    (h, c-1, connection point) for \\rho-shaped orbits, where h is the handle size and c the cycle size.*/
+template <Transformation F>
+std::tuple<DistanceType<F>, DistanceType<F>, Domain<F>> orbit_structure_nonterminating_orbit(Domain<F> x, F f) {
+    const auto y = connection_point_nonterminating_orbit(x, f);
+    return std::make_tuple(distance(x, y, f), distance(f(y), y, f), y);
 }
 
 /** \return (h-1, 0, terminal element) for terminating orbits, (0, c-1, x) for circular orbits and
     (h, c-1, connection point) for \\rho-shaped orbits, where h is the handle size and c the cycle size.*/
-template <Transformation F>
-std::tuple<DistanceType<F>, DistanceType<F>, Domain<F>> orbit_structure_nonterminating_orbit(const Domain<F>& x, F f) {
-    const auto y = connection_point_nonterminating_orbit(x, f);
-    return std::make_tuple(distance(x, y, f), distance(f(y), y, f), y);
+template <Transformation F, UnaryPredicate P>
+    requires std::same_as<Domain<F>, Domain<P>>
+std::tuple<DistanceType<F>, DistanceType<F>, Domain<F>> orbit_structure(Domain<F> x, F f, P p) {
+    // expect p(x) implies f(x) defined
+    using N = DistanceType<F>;
+    const auto y{connection_point(x, f, p)};
+    const auto m = distance(x, y, f);
+    const N n{};
+    if (p(y))
+        n = distance(f(y), y, f);
+    return std::make_tuple(m, n, y);
 }
 } // namespace mb::ul
 #endif
