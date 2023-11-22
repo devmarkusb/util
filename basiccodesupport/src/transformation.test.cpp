8256c75a21b217311b72fe8e6eeabd79d492006d
MarkusB <>
2023-11-23 00:05:47 +0100

    action impl and test, reorderings, fixes

--- basiccodesupport/src/transformation.test.cpp
+++ basiccodesupport/src/transformation.test.cpp
@@ -46,6 +46,27 @@ struct TransfEx {
         return x;
     }
 };
+
+template <std::integral I>
+struct ActionEx {
+    using N = ul::DistanceType<I>;
+
+    TransfExPredicate<I> p;
+
+    ActionEx(I x_0, N h, N c)
+        : p{x_0, h, c} {
+        UL_EXPECT(h >= 0 && c >= 0);
+        UL_EXPECT(
+            h < static_cast<N>(std::numeric_limits<I>::max()) && c < static_cast<N>(std::numeric_limits<I>::max()));
+    }
+
+    void operator()(I& x) const {
+        UL_EXPECT(p(x));
+        ++x;
+        if (x == p.x_0 + static_cast<I>(p.h) + static_cast<I>(p.c))
+            x = p.x_0 + static_cast<I>(p.h);
+    }
+};
 } // namespace
 
 namespace mb::ul {
@@ -71,7 +92,7 @@ INSTANTIATE_TEST_SUITE_P(
 
 // NOLINTEND
 
-TEST_P(OrbitAlgTest, misc) {
+TEST_P(OrbitAlgTest, comprehensive) {
     using F = TransfEx<int>;
     // just two compilation checks
     using T = ul::Domain<F>;
@@ -103,6 +124,7 @@ TEST_P(OrbitAlgTest, misc) {
         EXPECT_TRUE(m0 == h);
         EXPECT_TRUE(!m1);
         EXPECT_TRUE(m2 == ul::collision_point(x, f, f.p));
+        EXPECT_TRUE(m2 == y);
     } else if (!h) {
         // circular
         EXPECT_TRUE(!m0);
@@ -128,4 +150,69 @@ TEST_P(OrbitAlgTest, misc) {
             EXPECT_TRUE(m2 == y);
         }
     }
+}
+
+TEST_P(OrbitAlgTest, action) {
+    using Arg = int;
+    using F = ActionEx<Arg>;
+    const auto x{std::get<0>(GetParam())};
+    const auto h{std::get<1>(GetParam())};
+    const auto c{std::get<2>(GetParam())};
+    const F f{x, h, c};
+    EXPECT_TRUE(!c == ul::terminating_action<Arg>(x, f, f.p));
+    const auto y{ul::connection_point_action<Arg>(x, f, f.p)};
+    auto [m0, m1, m2]{ul::orbit_structure_action<Arg>(x, f, f.p)};
+    if (!c) {
+        // terminating
+        EXPECT_TRUE(m0 == h);
+        EXPECT_TRUE(!m1);
+        EXPECT_TRUE(m2 == y);
+    } else if (!h) {
+        // circular
+        EXPECT_TRUE(!m0);
+        EXPECT_TRUE(m1 == c - 1);
+        EXPECT_TRUE(m2 == x);
+    } else {
+        // rho-shaped
+        EXPECT_TRUE(m0 == h);
+        EXPECT_TRUE(m1 == c - 1);
+        EXPECT_TRUE(m2 == y);
+    }
+}
+
+TEST(distanceTest, misc) {
+    const TransfEx<int> f_c{0, 0u, 5u};
+    EXPECT_EQ(ul::distance(1, 3, f_c), 2u);
+    EXPECT_EQ(ul::distance(2, 1, f_c), 4u);
+    EXPECT_EQ(ul::distance(2, 2, f_c), 0u);
+    EXPECT_EQ(ul::distance(0, 0, f_c), 0u);
+    EXPECT_EQ(ul::distance(4, 4, f_c), 0u);
+    EXPECT_EQ(ul::distance(4, 0, f_c), 1u);
+
+    const TransfEx<int> f_r{0, 4u, 2u};
+    EXPECT_EQ(ul::distance(0, 5, f_r), 5u);
+
+    const TransfEx<int> f_t{0, 4u, 0u};
+    EXPECT_EQ(ul::distance(0, 4, f_t), 4u);
+
+    EXPECT_EQ(
+        ul::distance(
+            1, 5,
+            [](int x) {
+                return x + 1;
+            }),
+        4u);
+
+    static_assert(ul::Action<
+                  decltype([](int& x) {
+                      ++x;
+                  }),
+                  int&>);
+    EXPECT_EQ(
+        ul::distance_action<int>(
+            1, 5,
+            [](int& x) {
+                ++x;
+            }),
+        4u);
 }
