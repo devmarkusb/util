425cf6f807d756f1fd113e0c2d0070281962fa8e
MarkusB <>
2023-11-24 22:16:36 +0100

    cmake, tidy

--- buildenv/cmake_util/iwyu.cmake
+++ buildenv/cmake_util/iwyu.cmake
@@ -2,6 +2,6 @@
 # Usage:
 # set_property(TARGET target PROPERTY CXX_INCLUDE_WHAT_YOU_USE ${iwyu_path})
 
-cmake_minimum_required(VERSION 3.3 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
 
 find_program(iwyu_path NAMES include-what-you-use iwyu REQUIRED)
