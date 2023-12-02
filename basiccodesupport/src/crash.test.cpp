81443ce9fe6d18bc0962759a4aa2f696f656c063
MarkusB <>
2023-12-02 22:06:47 +0100

    win fix

--- basiccodesupport/src/crash.test.cpp
+++ basiccodesupport/src/crash.test.cpp
@@ -16,16 +16,23 @@ void exit(int status) {
 }
 } // namespace wrap
 
+#if UL_OS_WINDOWS
 TEST(crashTest, sigsegv) {
-    ASSERT_EXIT((ul::crash(SIGSEGV), wrap::exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
-    ASSERT_EXIT((ul::crash(SIGSEGV, false), wrap::exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
+    EXPECT_DEATH(ul::crash(SIGSEGV), ".*");
+    EXPECT_DEATH(ul::crash(SIGSEGV, false), ".*");
+}
+#else
+TEST(crashTest, sigsegv) {
+    EXPECT_EXIT((ul::crash(SIGSEGV), wrap::exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
+    EXPECT_EXIT((ul::crash(SIGSEGV, false), wrap::exit(0)), ::testing::KilledBySignal(SIGSEGV), ".*");
 }
 
 TEST(crashTest, sigabrt) {
-    ASSERT_EXIT((ul::crash(SIGABRT), wrap::exit(0)), ::testing::KilledBySignal(SIGABRT), ".*");
-    ASSERT_EXIT((ul::crash(SIGABRT, false), wrap::exit(0)), ::testing::KilledBySignal(SIGABRT), ".*");
+    EXPECT_EXIT((ul::crash(SIGABRT), wrap::exit(0)), ::testing::KilledBySignal(SIGABRT), ".*");
+    EXPECT_EXIT((ul::crash(SIGABRT, false), wrap::exit(0)), ::testing::KilledBySignal(SIGABRT), ".*");
 }
+#endif
 
 TEST(crashTest, not_crashing) {
-    ASSERT_EXIT((ul::crash(0), wrap::exit(0)), ::testing::ExitedWithCode(0), ".*");
+    EXPECT_EXIT((ul::crash(0), wrap::exit(0)), ::testing::ExitedWithCode(0), ".*");
 }
