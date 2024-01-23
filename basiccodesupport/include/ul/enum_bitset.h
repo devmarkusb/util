e51270fe3df699c360078a71a5d5ad985c90315c
MarkusB <>
2024-01-23 23:04:14 +0100

    tune+fix enum_bitset.h

--- basiccodesupport/include/ul/enum_bitset.h
+++ basiccodesupport/include/ul/enum_bitset.h
@@ -15,19 +15,24 @@
 
 #if __cpp_concepts && __cpp_lib_concepts
 namespace mb::ul {
-/** Wrap your enum EnumType into a type allowing logical combinations of the enum's elements by mapping the enum enum_values
-    to bit positions (starting with LSB). Requirements: The enum must contain an element called `end` being the last one
-    (having a enum_value greater than everyone else). The enum_value of `end` needs to be less than or equal the number of bits
-    within type SizeType plus 1. The enum should not contain enum_values smaller than 0 (at least you have to take care
-    yourself, that you don't do manipulations with it with this class). It is allowed to contain gaps
-    though.
-    Special case: the end enum_value is identified with 'empty' or 'no enum_value' or 0, which is the default initialization also.
+/** Wrap your enum EnumType into a type allowing logical combinations of the enum's elements by mapping the enum
+    enum_values to bit positions (starting with LSB). Requirements: The enum must contain an element called `end` being
+    the last one (having a enum_value greater than everyone else). The enum_value of `end` needs to be less than or
+    equal the number of bits within type SizeType plus 1. The enum should not contain enum_values smaller than 0 (at
+    least you have to take care yourself, that you don't do manipulations with it with this class). It is allowed to
+    contain gaps though.
+    Special case: the end enum_value could in principle be identified with 'empty' or 'no enum_value' or 0, but for
+    performance reasons it isn't. But you can view it as such, and it is the default initialization also. In follow-up
+    processing you can arrive there by calling clear also. Assigning end won't work.
+    Note, you should not strive for supporting bitwise combinations for any enum type to use them flag-like. It makes
+    sense to keep that a separate type, representing only a single value exclusively.
     */
 template <
     EnumConcept EnumType, std::integral SizeType = std::underlying_type_t<EnumType>, bool experimental_implicit = false>
+    requires requires { EnumType::end; }
 class EnumBitset {
 public:
-    static_assert(enum_cast(EnumType::end) <= sizeof(SizeType) * bits::bits_per_byte + 1);
+    static_assert(enum_cast(EnumType::end) <= sizeof(SizeType) * bits::bits_per_byte);
 
     using UnderlyingType = SizeType;
 
@@ -40,6 +45,11 @@ public:
         : bits_{enum_values_to_bitset(enum_values...)} {
     }
 
+    EnumBitset& operator=(EnumType enum_value) noexcept {
+        bits_ = enum_value_to_bitset(enum_value);
+        return *this;
+    }
+
     static constexpr EnumBitset from_bits(SizeType bits) noexcept {
         EnumBitset res;
         res.bits_ = bits;
@@ -48,6 +58,10 @@ public:
 
     static constexpr EnumBitset from_range(const Range auto& c) noexcept {
         return EnumBitset::from_bits(std::apply(enum_values_to_bitset, c));
+    }
+
+    constexpr void clear() noexcept {
+        bits_ = {};
     }
 
     [[nodiscard]] constexpr explicit(!experimental_implicit) operator bool() const noexcept {
@@ -62,6 +76,7 @@ public:
         return bits::count_set(bits_);
     }
 
+    // note, because of this we can't support EnumBitset-EnumType comparisons, but that won't be apples-apples anyway
     constexpr auto operator<=>(const EnumBitset&) const noexcept = default;
 
     constexpr EnumBitset operator&=(const EnumBitset& rhs) noexcept {
@@ -127,11 +142,11 @@ public:
     }
 
     friend constexpr EnumBitset operator|(const EnumType& lhs, const EnumBitset& rhs) noexcept {
-        return rhs & lhs;
+        return rhs | lhs;
     }
 
     friend constexpr EnumBitset operator^(const EnumType& lhs, const EnumBitset& rhs) noexcept {
-        return rhs & lhs;
+        return rhs ^ lhs;
     }
 
 private:
--- basiccodesupport/src/enum_bitset.test.cpp
+++ basiccodesupport/src/enum_bitset.test.cpp
@@ -36,6 +36,32 @@ TEST(EnumBitSet, construction) {
     EXPECT_TRUE(bitset3);
     EXPECT_EQ(bitset3.count(), 2);
     EXPECT_TRUE(bitset3.bits());
+
+    Ex1Bitset bitset4{bitset3};
+    EXPECT_EQ(bitset3, bitset4);
+
+    bitset3.clear();
+    EXPECT_FALSE(bitset3 & Ex1::first);
+    EXPECT_FALSE(bitset3);
+    EXPECT_FALSE(bitset3.count());
+    EXPECT_FALSE(bitset3.bits());
+
+    bitset2 = {};
+    EXPECT_FALSE(bitset2 & Ex1::first);
+    EXPECT_FALSE(bitset2);
+    EXPECT_FALSE(bitset2.count());
+    EXPECT_FALSE(bitset2.bits());
+}
+
+TEST(EnumBitSet, assignment) {
+    Ex1Bitset bitset{Ex1::third, Ex1::second};
+    Ex1Bitset bitset2;
+
+    bitset2 = bitset;
+    EXPECT_EQ(bitset, bitset2);
+
+    bitset2 = Ex1::first;
+    EXPECT_TRUE((bitset2 & Ex1::first) == bitset2);
 }
 
 TEST(EnumBitSet, operators) {
@@ -47,14 +73,39 @@ TEST(EnumBitSet, operators) {
     Ex1Bitset bitset3{Ex1::third, Ex1::second};
 
     EXPECT_EQ(bitset2 & Ex1::second, bitset);
+    EXPECT_EQ(Ex1::second & bitset2, bitset);
     EXPECT_EQ(bitset2 & bitset3, bitset2);
 
     EXPECT_EQ(bitset2 | Ex1::second, bitset3);
+    EXPECT_EQ(Ex1::second | bitset2, bitset3);
     EXPECT_EQ(bitset2 | bitset3, bitset3);
+
+    EXPECT_EQ(bitset2 ^ bitset3, Ex1Bitset{Ex1::second});
+    EXPECT_EQ(bitset3 ^ Ex1::third, Ex1Bitset{Ex1::second});
+    EXPECT_EQ(Ex1::third ^ bitset3, Ex1Bitset{Ex1::second});
+
+    EXPECT_TRUE(~bitset3 & Ex1Bitset{Ex1::first});
+    EXPECT_FALSE(~bitset3 & Ex1Bitset{Ex1::second});
+    EXPECT_FALSE(~bitset3 & Ex1Bitset{Ex1::third});
 
     EXPECT_NE(bitset2, bitset3);
 
     EXPECT_EQ(~bitset, Ex1Bitset::from_bits(~static_cast<Ex1Bitset::UnderlyingType>(0)));
+}
+
+TEST(EnumBitSet, comparisons) {
+    Ex1Bitset bitset{Ex1::first};
+    Ex1Bitset bitset2{Ex1::second, Ex1::third};
+    EXPECT_NE(bitset, bitset2);
+    EXPECT_TRUE(bitset < bitset2 || bitset > bitset2);
+    EXPECT_TRUE(bitset <= bitset2 || bitset >= bitset2);
+    EXPECT_FALSE((bitset <=> bitset2) == std::strong_ordering::equal);
+
+    auto bitset3{bitset2};
+    EXPECT_EQ(bitset3, bitset2);
+    EXPECT_FALSE(bitset3 < bitset2 || bitset3 > bitset2);
+    EXPECT_TRUE(bitset3 <= bitset2 && bitset3 >= bitset2);
+    EXPECT_TRUE((bitset3 <=> bitset2) == std::strong_ordering::equal);
 }
 
 TEST(EnumBitSet, fromRange) {
@@ -80,4 +131,35 @@ TEST(EnumBitSet, signedEnum) {
     EnumBitset<SignedEnum> bitset{SignedEnum::pos};
     EXPECT_TRUE(bitset);
     EXPECT_TRUE((bitset & SignedEnum::pos) == bitset);
+}
+
+enum class Ex2 : uint8_t {
+    first,
+    second,
+    third = 7,
+    end
+};
+
+enum class Ex3 : uint8_t {
+    first,
+    second,
+    third = 8,
+    end
+};
+
+TEST(EnumBitSet, size) {
+    EnumBitset<Ex2> bitset{Ex2::third};
+    EXPECT_TRUE((bitset & Ex2::third) == bitset);
+
+    EnumBitset<Ex2, uint16_t> bitset2{Ex2::third};
+    EXPECT_TRUE((bitset2 & Ex2::third) == bitset2);
+
+    // not representable in uint8_t anymore - try, won't compile
+#if 1
+    using S = uint16_t;
+#else
+    using S = uint8_t;
+#endif
+    EnumBitset<Ex3, S> bitset3{Ex3::third};
+    EXPECT_TRUE((bitset3 & Ex3::third) == bitset3);
 }
