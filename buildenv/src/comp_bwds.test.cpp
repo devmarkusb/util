8fe571c0857d6bb831b1cdc370a024b34bbd987c
MarkusB <>
2025-04-17 09:01:22 +0200

    up cli11

--- buildenv/src/comp_bwds.test.cpp
+++ buildenv/src/comp_bwds.test.cpp
@@ -1,1 +1,26 @@
-#include "ul/comp_bwds.h" // NOLINT
+#include "ul/comp_bwds.h"
+#include "gtest/gtest.h"
+#include <string>
+
+TEST(CompilerBackwardsTest, StringConversions) {
+    // Test string conversion functions if available
+#ifdef UL_HAS_STOI
+    EXPECT_EQ(42, std::stoi("42"));
+#endif
+#ifdef UL_HAS_STOLD
+    EXPECT_EQ(42.0L, std::stold("42.0"));
+#endif
+#ifdef UL_HAS_TO_STRING
+    EXPECT_EQ("42", std::to_string(42));
+#endif
+}
+
+TEST(CompilerBackwardsTest, MathFunctions) {
+    // Test math functions if available
+#ifdef UL_HAS_LLROUND
+    EXPECT_EQ(42, std::llround(42.3));
+    EXPECT_EQ(43, std::llround(42.7));
+    EXPECT_EQ(-42, std::llround(-42.3));
+    EXPECT_EQ(-43, std::llround(-42.7));
+#endif
+}
--- buildenv/src/warnings.test.cpp
+++ buildenv/src/warnings.test.cpp
@@ -1,1 +1,27 @@
-#include "ul/warnings.h" // NOLINT
+#include "ul/warnings.h"
+#include "gtest/gtest.h"
+
+TEST(WarningsTest, CompilerSpecific) {
+    UL_PRAGMA_WARNINGS_PUSH
+#if UL_COMP_MS_VISUAL_STUDIO_CPP
+    UL_WARNING_DISABLE_MSVC(4189);  // 'identifier': local variable is initialized but not referenced
+#elif UL_COMP_CLANG
+    UL_WARNING_DISABLE_CLANG(unused-variable);
+#elif UL_COMP_GNU_CPP
+    UL_WARNING_DISABLE_GCC(unused-variable);
+#endif
+    {
+        int unused = 42;  // Should not trigger warnings
+    }
+    UL_PRAGMA_WARNINGS_POP
+}
+
+TEST(WarningsTest, DisableAllMSVC) {
+#if UL_COMP_MS_VISUAL_STUDIO_CPP
+    UL_PRAGMA_WARNINGS_PUSH_AND_DISABLE_ALL_MSVC
+    {
+        int unused = 42;  // Should not trigger any warnings
+    }
+    UL_PRAGMA_WARNINGS_POP
+#endif
+}
