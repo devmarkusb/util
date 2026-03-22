dc7fea65c9e0324b2dce0c0b2e9ccbb053ddfec0
MarkusB <>
2026-03-22 09:36:24 +0100

    Refactor `new_statistics` to remove `UL_I_AM_SURE_TO_REPLACE_NEW_DELETE` macro and improve `str_convert` structure implementation.

--- mem/src/new_statistics.applied.test.cpp
+++ mem/src/new_statistics.applied.test.cpp
@@ -1,8 +1,6 @@
 #include "ul/assert.h"
 #include "ul/mem/types.h"
-#define UL_I_AM_SURE_TO_REPLACE_NEW_DELETE 1
 #include "ul/mem/new_statistics.h"
-#undef UL_I_AM_SURE_TO_REPLACE_NEW_DELETE
 #include "ul/error.h"
 #include "ul/format_number.h"
 #include "ul/macros.h"
