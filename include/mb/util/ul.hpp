6c524e9af705eea94de16e66c384fde4e80bde39
MarkusB <>
2026-03-27 00:20:18 +0100

    fix install test

---
+++ include/mb/util/ul.hpp
@@ -0,0 +1,10 @@
+#ifndef MB_UTIL_UL_HPP_9D2A9A80D85E4A4FBD41C3F96E90A3F1
+#define MB_UTIL_UL_HPP_9D2A9A80D85E4A4FBD41C3F96E90A3F1
+
+#include "ul/ul.hpp"
+
+namespace mb {
+namespace util = ul;
+} // namespace mb
+
+#endif
