2802dba63d04753fb7512c8cd9ede0b76913955f
MarkusB <>
2026-04-06 00:19:13 +0200

    Refactor CMake scripts to use MB_DEVENV_USING_LIBCPP.
    Replaced MB_UL_CPP_STD_LIB and related configurations with MB_DEVENV_USING_LIBCPP for simplified and consistent libc++ handling. Removed redundant sanitizer and stack protection options and cleaned up obsolete configuration logic.

--- cmake/cmake-util/googlebenchmark.cmake
+++ cmake/cmake-util/googlebenchmark.cmake
@@ -340,6 +340,6 @@ target_compile_options(
     benchmark
     PUBLIC ${MB_UL_google_benchmark_compile_options}
 )
-if("${MB_UL_CPP_STD_LIB}" STREQUAL "libc++")
+if(MB_DEVENV_USING_LIBCPP)
     add_definitions(-DBENCHMARK_USE_LIBCXX=ON)
 endif()
