6d98a0bf5f1e83c2b7e4cafcfb0a4b0e20563bc1
MarkusB <>
2026-04-06 00:19:13 +0200

    Refactor CMake scripts to use MB_DEVENV_USING_LIBCPP.
    Replaced MB_UL_CPP_STD_LIB and related configurations with MB_DEVENV_USING_LIBCPP for simplified and consistent libc++ handling. Removed redundant sanitizer and stack protection options and cleaned up obsolete configuration logic.

--- cmake/cmake-util/cpp-std-lib.cmake
+++ cmake/cmake-util/cpp-std-lib.cmake
@@ -1,29 +1,8 @@
-# Declares variables MB_UL_USE_CLANG_STDLIB and MB_UL_CPP_STD_LIB to set and detect the C++ standard library type to be used
-# for linking currently. Possible values are "libstdc++" for the GNU lib, "libc++" for the Clang lib and "msvc" for
-# Microsoft's implementation.
-
-include(CheckCXXSourceCompiles)
+# Print binaries involved when MB_DEVENV_USING_LIBCPP (libc++ is used)
 
 if(MB_UL_ANDROID)
     return()
 endif()
-
-if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
-    set(default_use_clang_stdlib ON)
-else()
-    set(default_use_clang_stdlib OFF)
-endif()
-
-# Doesn't work reliably, also manually specify -DCMAKE_CXX_FLAGS="-stdlib=libc++"
-# as some compiler tests seem to start before anything else.
-option(
-    MB_UL_USE_CLANG_STDLIB
-    "use libc++ instead of libstdc++"
-    ${default_use_clang_stdlib}
-)
-
-# default, to be overwritten in case
-set(MB_UL_CPP_STD_LIB_IMPL "libstdc++")
 
 # C++ standard library setup
 # Use libc++ with clang and libstdc++ with gcc
@@ -32,13 +11,8 @@ if(
         "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"
         OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang"
     )
-    AND MB_UL_USE_CLANG_STDLIB
+    AND MB_DEVENV_USING_LIBCPP
 )
-    set(STDLIB_CXX_FLAGS "-stdlib=libc++" CACHE STRING "internal")
-    # to use the clang c++ stdlib we must set the flag -stdlib=libc++
-    # see https://libcxx.llvm.org/docs/UsingLibcxx.html
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${STDLIB_CXX_FLAGS}")
-
     execute_process(
         COMMAND ${CMAKE_CXX_COMPILER} -print-effective-triple
         OUTPUT_VARIABLE triple
@@ -58,7 +32,6 @@ if(
         find_library(clang_stdlib NAMES c++)
     endif()
     if(clang_stdlib)
-        set(MB_UL_CPP_STD_LIB_IMPL "libc++")
         message(STATUS "Found libc++: ${clang_stdlib}")
         get_filename_component(clang_stdlib_directory ${clang_stdlib} DIRECTORY)
         get_filename_component(clang_stdlib_fname ${clang_stdlib} NAME)
@@ -82,62 +55,4 @@ if(
         # these perhaps need to be installed together with app binaries
         cmake_print_variables(clang_stdlib_files clang_stdlib_abi_files)
     endif()
-elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    set(MB_UL_CPP_STD_LIB_IMPL "msvc")
 endif()
-
-check_cxx_source_compiles(
-    "
-    #include <cstddef>
-    int a =
-    #ifdef _LIBCPP_VERSION
-        1;
-    #else
-        somethingthatdoesntcompile;
-    #endif
-    int main() { return 0; }
-    "
-    MB_UL_CHECK_USING_CLANG_CPP_STD_LIB
-)
-
-check_cxx_source_compiles(
-    "
-    #include <cstddef>
-    int a =
-    #ifdef __GLIBCXX__
-        1;
-    #else
-        somethingthatdoesntcompile;
-    #endif
-    int main() { return 0; }
-    "
-    MB_UL_CHECK_USING_GNU_CPP_STD_LIB
-)
-
-if(MB_UL_CHECK_USING_CLANG_CPP_STD_LIB)
-    set(MB_UL_CPP_STD_LIB_IMPL2 "libc++")
-elseif(MB_UL_CHECK_USING_GNU_CPP_STD_LIB)
-    set(MB_UL_CPP_STD_LIB_IMPL2 "libstdc++")
-elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-    set(MB_UL_CPP_STD_LIB_IMPL2 "msvc")
-else()
-    set(MB_UL_CPP_STD_LIB_IMPL2 "?")
-endif()
-
-if(NOT "${MB_UL_CPP_STD_LIB_IMPL2}" STREQUAL "${MB_UL_CPP_STD_LIB_IMPL}")
-    cmake_print_variables(MB_UL_CPP_STD_LIB_IMPL2)
-    cmake_print_variables(MB_UL_CPP_STD_LIB_IMPL)
-    message(
-        FATAL_ERROR
-        "Above vars don't match, consistency check needs update."
-    )
-endif()
-
-set(MB_UL_CPP_STD_LIB
-    ${MB_UL_CPP_STD_LIB_IMPL}
-    CACHE INTERNAL
-    "indicates C++ standard library currently in use"
-    FORCE
-)
-
-cmake_print_variables(MB_UL_CPP_STD_LIB)
--- cmake/cmake-util/diagnostics.cmake
+++ cmake/cmake-util/diagnostics.cmake
@@ -6,92 +6,12 @@ include_guard(DIRECTORY)
 
 option(MB_UL_COVERAGE "coverage" OFF)
 
-# sanitizers, mutually exclusive for now
-option(MB_UL_ADDRESS_SAN "address sanitizer (asan)" OFF)
-option(MB_UL_THREAD_SAN "thread sanitizer" OFF)
-option(MB_UL_MEMORY_SAN "memory sanitizer" OFF)
-option(MB_UL_UNDEF_SAN "undefined behavior sanitizer" OFF)
-option(
-    MB_UL_ENABLE_STACK_PROTECTION
-    "enables compiler settings to protect stack corruptions"
-    OFF
-)
-
-if("${MB_UL_CPP_STD_LIB}" STREQUAL "libc++")
+if(MB_DEVENV_USING_LIBCPP)
     option(
         MB_UL_LIBCPP_DEBUG
         "enables the _LIBCPP_DEBUG mode, prerequisite: compiler being built with LIBCXX_ENABLE_DEBUG_MODE_SUPPORT"
         OFF
     )
-endif()
-
-if(MB_UL_ADDRESS_SAN)
-    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        # nothing yet
-        message(
-            FATAL_ERROR
-            "MB_UL_ADDRESS_SAN not implemented for GNU compiler"
-        )
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-        # nothing yet
-        message(
-            FATAL_ERROR
-            "MB_UL_ADDRESS_SAN not implemented for MSVC compiler"
-        )
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(CMAKE_CXX_FLAGS
-            "${CMAKE_CXX_FLAGS} -fsanitize=address \
--O1 -g -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-common"
-        )
-        set(CMAKE_EXE_LINKER_FLAGS
-            "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=address"
-        )
-    endif()
-elseif(MB_UL_THREAD_SAN)
-    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        # nothing yet
-        message(FATAL_ERROR "MB_UL_THREAD_SAN not implemented for GNU compiler")
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-        # nothing yet
-        message(
-            FATAL_ERROR
-            "MB_UL_THREAD_SAN not implemented for MSVC compiler"
-        )
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=thread")
-        set(CMAKE_EXE_LINKER_FLAGS
-            "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=thread"
-        )
-    endif()
-elseif(MB_UL_MEMORY_SAN)
-    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        # nothing yet
-        message(FATAL_ERROR "MB_UL_MEMORY_SAN not implemented for GNU compiler")
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-        # nothing yet
-        message(
-            FATAL_ERROR
-            "MB_UL_MEMORY_SAN not implemented for MSVC compiler"
-        )
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=memory")
-        set(CMAKE_EXE_LINKER_FLAGS
-            "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=memory"
-        )
-    endif()
-elseif(MB_UL_UNDEF_SAN)
-    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        # nothing yet
-        message(FATAL_ERROR "MB_UL_UNDEF_SAN not implemented for GNU compiler")
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-        # nothing yet
-        message(FATAL_ERROR "MB_UL_UNDEF_SAN not implemented for MSVC compiler")
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=undefined")
-        set(CMAKE_EXE_LINKER_FLAGS
-            "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=undefined"
-        )
-    endif()
 endif()
 
 if(MB_UL_COVERAGE)
@@ -139,8 +59,3 @@ endif()
 if(MB_UL_LIBCPP_DEBUG)
     add_definitions(-D_LIBCPP_DEBUG=1)
 endif()
-
-if(MB_UL_ENABLE_STACK_PROTECTION)
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fstack-protector-all")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fstack-protector-all")
-endif(MB_UL_ENABLE_STACK_PROTECTION)
