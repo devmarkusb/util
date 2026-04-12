ffe463fbf1b1d53160081a19e09eff4beed15b6f
MarkusB <>
2026-04-12 14:22:40 +0200

    Integrate `gsl` and enhance `main` example with `ul` features.

--- examples/usage.cpp
+++ examples/usage.cpp
@@ -1,5 +1,26 @@
 #include "mb/ul/ul.hpp"
 
+#include "gsl/gsl"
+#include <iostream>
+#include <print>
+
 int main() {
-    return 0;
+    try {
+        // random feature from ul
+        constexpr auto example_nr{static_cast<int32_t>(std::numeric_limits<int8_t>::max())};
+        std::println("narrowing {} int to int8_t: {}", example_nr, ul::narrow<int8_t>(example_nr));
+
+        // random feature from gsl
+        gsl::owner<int*> p{new int{42}}; // NOLINT // NOSONAR
+        ul::finally([&p]() {
+            delete p; // NOLINT // NOSONAR
+        });
+        UL_ASSERT(p);
+        std::println("p: {}", *p);
+
+        return ul::prog_exit_success;
+    } catch (const std::exception& e) {
+        std::cerr << "Caught exception: " << e.what() << "\n";
+        return ul::prog_exit_failure;
+    }
 }
