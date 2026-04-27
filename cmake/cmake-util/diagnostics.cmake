c8725bb02728f09a5dedcf938b9c5137ba6441b0
MarkusB <>
2026-04-28 00:51:50 +0200

    cleanup cmake utils

--- cmake/cmake-util/diagnostics.cmake
+++ cmake/cmake-util/diagnostics.cmake
@@ -4,56 +4,12 @@
 
 include_guard(DIRECTORY)
 
-option(MB_UL_COVERAGE "coverage" OFF)
-
 if(MB_DEVENV_USING_LIBCPP)
     option(
         MB_UL_LIBCPP_DEBUG
         "enables the _LIBCPP_DEBUG mode, prerequisite: compiler being built with LIBCXX_ENABLE_DEBUG_MODE_SUPPORT"
         OFF
     )
-endif()
-
-if(MB_UL_COVERAGE)
-    if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
-        message(
-            FATAL_ERROR
-            "MB_UL_COVERAGE only working for debug builds, at least non-optimized -O0"
-        )
-    endif()
-    if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-        set(CMAKE_MODULE_PATH
-            ${CMAKE_MODULE_PATH}
-            "${CMAKE_CURRENT_LIST_DIR}/3rdparty/bilke/cmake-modules"
-            CACHE STRING
-            ""
-            FORCE
-        )
-        #include(CodeCoverage)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
-    elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-        # also not working: -fcoverage-mapping -fprofile-instr-generate
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -coverage")
-    else()
-        message(FATAL_ERROR "MB_UL_COVERAGE not implemented for this compiler")
-    endif()
-    if(
-        "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU"
-        OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang"
-    )
-        find_program(LCOV lcov REQUIRED)
-        find_program(GENHTML genhtml REQUIRED)
-
-        # add coverage target
-        add_custom_target(
-            coverage
-            # gather data
-            COMMAND ${LCOV} --directory . --capture --output-file coverage.info
-            # generate report
-            COMMAND ${GENHTML} --demangle-cpp -o coverage coverage.info
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-        )
-    endif()
 endif()
 
 if(MB_UL_LIBCPP_DEBUG)
--- cmake/cmake-util/githooks.cmake
+++
@@ -1,7 +0,0 @@
-# include in your project root once; enables dir .githooks in project root as a place for git hooks under version
-# control, opposed to .git/hooks
-
-# in order to work for submodules also I found no other way than to use --global
-execute_process(
-    COMMAND git config --global core.hookspath .githooks
-)
