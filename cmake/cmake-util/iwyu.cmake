0d1ba7199dcf6e944dfec503aedb60e6c15fb8cd
MarkusB <>
2026-04-28 00:51:50 +0200

    cleanup cmake utils

--- cmake/cmake-util/iwyu.cmake
+++
@@ -1,7 +0,0 @@
-# Not yet working, don't know why.
-# Usage:
-# set_property(TARGET target PROPERTY CXX_INCLUDE_WHAT_YOU_USE ${iwyu_path})
-
-cmake_minimum_required(VERSION 3.5 FATAL_ERROR)
-
-find_program(iwyu_path NAMES include-what-you-use iwyu REQUIRED)
