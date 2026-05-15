25600a834d745a9e94fa5b082dd5d598c980db12
MarkusB <>
2026-05-15 22:54:24 +0200

    fix: gcc warning suppression

--- include/mb/ul/mem/new_handler.hpp
+++ include/mb/ul/mem/new_handler.hpp
@@ -3,6 +3,7 @@
 #ifndef NEW_HANDLER_H_N2IO38ZRE2387XNZ27R
 #define NEW_HANDLER_H_N2IO38ZRE2387XNZ27R
 
+#include "mb/ul/buildenv/warnings.hpp"
 #include <cstddef>
 #include <new>
 #include <utility>
@@ -17,8 +18,13 @@ public:
     //! With this method you can assign a "void OutOfMem()" function to your class.
     /** This is much more convenient than wrapping every new-call in a try block.*/
     static std::new_handler set_new_handler(std::new_handler p);
-//! Class specific new, responsible for setting up the custom new handler and resetting back to the global default.
-#undef new
+    //! Class specific new, responsible for setting up the custom new handler and resetting back to the global default.
+    UL_PRAGMA_WARNINGS_PUSH
+    // clang-format off
+    UL_WARNING_DISABLE_GCC(keyword-macro)
+    // clang-format on
+    #undef new
+    UL_PRAGMA_WARNINGS_POP
     static void* operator new(size_t size);
     static void operator delete(void* ptr) noexcept;
     static void operator delete(void* ptr, size_t size) noexcept;
