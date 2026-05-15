903782c805e5b121c03cdcda27267fd6e8596fd5
MarkusB <>
2026-05-15 22:54:24 +0200

    fix: gcc warning suppression
    (cherry picked from commit 25600a834d745a9e94fa5b082dd5d598c980db12)

--- mem/include/ul/mem/new_handler.h
+++ mem/include/ul/mem/new_handler.h
@@ -3,8 +3,10 @@
 #ifndef NEW_HANDLER_H_N2IO38ZRE2387XNZ27R
 #define NEW_HANDLER_H_N2IO38ZRE2387XNZ27R
 
+#include "mb/ul/buildenv/warnings.hpp"
 #include <cstddef>
 #include <new>
+#include <utility>
 
 namespace mb::ul::mem {
 //! Provides a base class to equip arbitrary classes with their *own* new handler support.
@@ -16,9 +18,16 @@ public:
     //! With this method you can assign a "void OutOfMem()" function to your class.
     /** This is much more convenient than wrapping every new-call in a try block.*/
     static std::new_handler set_new_handler(std::new_handler p);
-//! Class specific new, responsible for setting up the custom new handler and resetting back to the global default.
-#undef new
-    void* operator new(size_t size);
+    //! Class specific new, responsible for setting up the custom new handler and resetting back to the global default.
+    UL_PRAGMA_WARNINGS_PUSH
+    // clang-format off
+    UL_WARNING_DISABLE_GCC(keyword-macro)
+    // clang-format on
+    #undef new
+    UL_PRAGMA_WARNINGS_POP
+    static void* operator new(size_t size);
+    static void operator delete(void* ptr) noexcept;
+    static void operator delete(void* ptr, size_t size) noexcept;
 
 private:
     //! Class specific new handler.
@@ -32,9 +41,7 @@ private:
 
 template <typename T>
 std::new_handler NewHandlerSupport<T>::set_new_handler(std::new_handler p) {
-    std::new_handler old = current_handler();
-    current_handler() = p;
-    return old;
+    return std::exchange(current_handler(), p);
 }
 
 template <typename T>
@@ -49,6 +56,16 @@ void* NewHandlerSupport<T>::operator new(size_t size) {
     }
     std::set_new_handler(global_handler);
     return memory;
+}
+
+template <typename T>
+void NewHandlerSupport<T>::operator delete(void* ptr) noexcept {
+    ::operator delete(ptr);
+}
+
+template <typename T>
+void NewHandlerSupport<T>::operator delete(void* ptr, size_t /*size*/) noexcept {
+    ::operator delete(ptr);
 }
 } // namespace mb::ul::mem
 
