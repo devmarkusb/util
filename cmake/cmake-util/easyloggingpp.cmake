6f0a2712b76b47acf59b96f75e2ecb5f8facfaf2
MarkusB <>
2026-05-28 13:09:18 +0200

    Update compiler warning suppression logic in CMake scripts

--- cmake/cmake-util/easyloggingpp.cmake
+++ cmake/cmake-util/easyloggingpp.cmake
@@ -29,31 +29,14 @@ if(MSVC)
     set_source_files_properties(
         ${easyloggingpp_SOURCE_FILES}
         DIRECTORY ${CMAKE_SOURCE_DIR}
-        PROPERTIES COMPILE_DEFINITIONS "WIN32_LEAN_AND_MEAN;ELPP_WINSOCK2"
-    )
-elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    string(
-        CONCAT compileOptions
-        "-Wno-sign-conversion;-Wno-implicit-int-conversion;"
-        "-Wno-unused-parameter"
-    )
-    if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 10)
-        string(
-            CONCAT compileOptions
-            "${compileOptions};-Wno-implicit-int-float-conversion;-Wno-range-loop-construct"
-        )
-    endif()
-    set_source_files_properties(
-        ${easyloggingpp_SOURCE_FILES}
-        DIRECTORY ${CMAKE_SOURCE_DIR}
-        PROPERTIES COMPILE_OPTIONS "${compileOptions}"
-    )
-elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-    set_source_files_properties(
-        ${easyloggingpp_SOURCE_FILES}
-        DIRECTORY ${CMAKE_SOURCE_DIR}
         PROPERTIES
-            COMPILE_OPTIONS
-                "-Wno-deprecated-declarations;-Wno-conversion;-Wno-unused-parameter;-Wno-range-loop-construct"
+            COMPILE_DEFINITIONS "WIN32_LEAN_AND_MEAN;ELPP_WINSOCK2"
+            COMPILE_OPTIONS /w
+    )
+elseif(CMAKE_CXX_COMPILER_ID MATCHES "AppleClang|Clang|GNU")
+    set_source_files_properties(
+        ${easyloggingpp_SOURCE_FILES}
+        DIRECTORY ${CMAKE_SOURCE_DIR}
+        PROPERTIES COMPILE_OPTIONS -w
     )
 endif()
